f43f0bf1c1b6e22401b7717e40f0e4a84690461e
Jahoon Lee <aoleejohn@naver.com>
2022-11-14 11:12:11 +0900

    1

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,35 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/13 14:37:44 by jahlee            #+#    #+#             */
-/*   Updated: 2022/11/14 11:11:37 by jahlee           ###   ########.fr       */
+/*   Created: 2022/11/14 10:01:22 by jahlee            #+#    #+#             */
+/*   Updated: 2022/11/14 11:11:52 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
-
-int	ft_memcmp(const void *b1, const void *b2, size_t n)
-{
-	size_t			i;
-	unsigned char	*s1;
-	unsigned char	*s2;
-
-	s1 = (unsigned char *)b1;
-	s2 = (unsigned char *)b2;
-	i = 0;
-	while (n--)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	return (0);
-}
 
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -52,15 +33,4 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
         i++;
 	}
 	return (0);
-}
-
-
-int	main()
-{
-	char	str1[100] = "123456789";
-	char	str2[100] = "7";
-
-	printf("ft     : %s\n",ft_strnstr(str1,str2,7));
-	// printf("origin : %d\n",strnstr(str1,str2,10));
-
 }
