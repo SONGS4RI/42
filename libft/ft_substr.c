a8a9ecffe07605e2233c016459113817caecd296
Jahoon Lee <aoleejohn@naver.com>
2022-11-15 14:19:47 +0900

    1

---
+++ libft/ft_substr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 14:16:23 by jahlee            #+#    #+#             */
+/*   Updated: 2022/11/15 14:19:17 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*mem;
+
+	if (mem = malloc(sizeof(char) * len))
+		return (NULL);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:14:49 by jahlee            #+#    #+#             */
-/*   Updated: 2022/11/13 20:44:00 by jahlee           ###   ########.fr       */
+/*   Updated: 2022/11/15 14:16:41 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,5 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
