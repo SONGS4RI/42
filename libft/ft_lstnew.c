966c5ec6f3630b4be19c5b6ba1b7b8dd014b6ac9
Jahoon Lee <aoleejohn@naver.com>
2022-11-21 15:44:32 +0900

    1

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,13 +6,20 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:44:37 by jahlee            #+#    #+#             */
-/*   Updated: 2022/11/21 14:44:57 by jahlee           ###   ########.fr       */
+/*   Updated: 2022/11/21 15:23:37 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
