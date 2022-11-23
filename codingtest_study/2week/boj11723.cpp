5e2ee50c9534481268b20daefd42919a976e35bc
Jahoon Lee <jahlee@student.42seoul.kr>
2022-11-23 10:53:30 +0900

    1

--- codingtest_study/2week/boj11723.cpp
+++ codingtest_study/2week/boj11723.cpp
@@ -1,43 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    set<int> arr;
+	ios::sync_with_stdio(0);
+    cin.tie(0);
     string s;
-    int num,n;
-
-    cin >> n;
-    while(n--)
+    int a[21] = {0,},m,b;
+	cin >> m;
+    while (m--)
     {
-        cin >> s;
-        if (s == "add")
+		cin >> s;
+		if (s == "add")
         {
-            cin >> num;
-            arr.insert(num);
-        }
-        else if(s == "remove")
+			cin >> b;
+            a[b] = 1;
+		}
+		else if (s == "remove")
         {
-            cin >> num;
-            arr.erase(num);
-        }
-        else if(s == "check")
+			cin >> b;
+            a[b] = 0;
+		}
+		else if (s == "check")
         {
-            cin >> num;
-            cout << arr.count(num) << "\n";
-        }
-        else if(s == "toggle")
+			cin >> b;
+            cout << a[b] << '\n';
+		}
+		else if (s == "toggle")
         {
-            cin >> num;
-            if(arr.count(num))
-                arr.erase(num);
-            else
-                arr.insert(num);
-        }
-        else if(s == "all")
-            for(int i=1;i<=20;i++)
-                arr.insert(i);
-        else if(s == "empty")
-            arr.clear();
-    }
+			cin >> b;
+            a[b] = 1 - a[b];
+		}
+		else if (s == "all") fill(a, a + 21, 1);
+		else fill(a, a + 21, 0);
+	}
 }
+
+///////////다른 풀이_비트마스크////////////////
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main(){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0); cout.tie(0);
+
+//     int S=0;
+//     int M; cin>>M;
+
+//     while(M--){
+//         int x;
+//         string str; cin>>str;
+//         if(str=="add"){
+//             cin>>x;
+//             S|=(1<<x);
+//         }
+//         else if(str=="remove"){
+//             cin>>x;
+//             S&=~(1<<x);
+//         }
+//         else if(str=="check"){
+//             cin>>x;
+//             cout<<(S&(1<<x)?1:0)<<'\n';
+//         }
+//         else if(str=="toggle"){
+//             cin>>x;
+//             S^=(1<<x);
+//         }
+//         else if(str=="all"){
+//             S=(1<<21)-1;
+//         }
+//         else S=0;
+//     }
+
+//     return 0;
+// }
