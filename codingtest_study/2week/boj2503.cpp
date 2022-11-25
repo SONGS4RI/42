3f1a4a8224810a9c434e8475a3e78efacc4bcc77
Jahoon Lee <aoleejohn@naver.com>
2022-11-25 16:59:35 +0900

    1

--- codingtest_study/2week/boj2503.cpp
+++ codingtest_study/2week/boj2503.cpp
@@ -1,46 +1,77 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
 using namespace std;
+    
+int n, res=0;
+string s[101];
 
-int is_strike(string target, string res)
+int is_strike(string target, string ex)
 {
 	int strike = 0;
 	for(int i=0;i<3;i++)
 	{
-		if(res[i] == target[i])
+		if(ex[i] == target[i])
 			strike++;
 	}
 	return (strike);
 }
 
-int is_ball(string target, string res)
+int is_ball(string target, string ex)
 {
 	int ball = 0;
 	for(int i=0;i<3;i++)
 	{
 		for(int j=0;j<3;j++)
-			if(res[i] == target[j] && i!=j)
+			if(ex[i] == target[j] && i!=j)
 				ball++;
 	}
 	return (ball);
 }
 
-
+void dfs(int cnt, string ex)
+{
+	if (cnt == 3)
+	{
+		for (int i=0;i<n;i++)
+		{
+			cout << ex << "\n";
+			cout << is_strike(s[i],ex) << " : " << s[i][3] <<"\n";
+			cout << is_ball(s[i],ex)   << " : " << s[i][4]<< "\n";
+			if ((is_strike(s[i],ex) != s[i][3]) || (is_ball(s[i],ex) != s[i][4]))
+				return ;
+		}
+		res++;
+		return ;
+	}
+	for(int i=1;i<=9;i++)
+	{
+		int err = 0;
+		for(int j=0;j<cnt;j++)
+		{
+			if(ex[j] == i + '0')
+				err = 1;
+		}
+		if(!err)		
+		{
+			ex[cnt] = i + '0';
+			dfs(cnt+1, ex);
+		}
+	}
+	return ;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-	int n;
-	string s[101];
 
+	string ex = "000";
 	cin >> n;
 	for(int i=0;i<n;i++)
 	{
 		cin >> s[i];
-		for(int j=0;j<2;j++)
-			cin >> s[i][3 + j];
+		cin >> s[i][3];
+		cin >> s[i][4];
 	}
-	
-
+	dfs(0, ex);
+	cout << res << "\n";
 }
