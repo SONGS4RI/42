750befbaa7cdf4938f63be8781ac024803623f47
Jahoon Lee <aoleejohn@naver.com>
2022-11-25 15:03:45 +0900

    1

--- codingtest_study/2week/boj2503.cpp
+++ codingtest_study/2week/boj2503.cpp
@@ -1,21 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   boj2503.cpp                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/23 16:17:32 by jahlee            #+#    #+#             */
-/*   Updated: 2022/11/23 17:12:30 by jahlee           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <bits/stdc++>
 using namespace std;
+
+int is_strike(string target, string res)
+{
+	int strike = 0;
+	for(int i=0;i<3;i++)
+	{
+		if(res[i] == target[i])
+			strike++;
+	}
+	return (strike);
+}
+
+int is_ball(string target, string res)
+{
+	int ball = 0;
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+			if(res[i] == target[j] && i!=j)
+				ball++;
+	}
+	return (ball);
+}
+
+
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
+	int n;
+	string s[101];
+
+	cin >> n;
+	for(int i=0;i<n;i++)
+	{
+		cin >> s[i];
+		for(int j=0;j<2;j++)
+			cin >> s[i][3 + j];
+	}
+	
+
 }
