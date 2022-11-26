ce1c29135860be10ea7e01d5dbafa78bd67b0c60
Jahoon Lee <aoleejohn@naver.com>
2022-11-26 17:43:12 +0900

    1

--- codingtest_study/2week/boj1032.cpp
+++ codingtest_study/2week/boj1032.cpp
@@ -1,8 +1,8 @@
-#include <bits/stdc++.h> // 통합 헤더 (많이쓰는 헤더를 전부 포함하고있다)
-using namespace std; // (std 클래스의 특정함수들을 사용하겠다는 의미)
-                     // 만약 이렇게 쓰지않으면 std 함수들을 사용할때 
-int main()           // 범위 지정 연산자 (::)을 사용해주어야함
-{                    // ex) std::cout
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{                  
     int n, i;
     string s1,res;
 
