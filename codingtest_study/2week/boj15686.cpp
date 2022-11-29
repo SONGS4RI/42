aad740419aa02ccae99141f031d5d5ed8df89c6b
Jahoon Lee <aoleejohn@naver.com>
2022-11-29 15:04:39 +0900

    1

--- codingtest_study/2week/boj15686.cpp
+++ codingtest_study/2week/boj15686.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main()// 너비탐색
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int n,m;
+	cin >> n >> m;
+	int board[n+1][n+1];
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			cin >> board[i][j];
 	
 }
