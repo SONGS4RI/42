275b8b9db42db2a41195a44015a50daa059dc764
Jahoon Lee <jahlee@student.42seoul.kr>
2022-11-30 15:35:01 +0900

    1

--- codingtest_study/3week/boj2417.cpp
+++ codingtest_study/3week/boj2417.cpp
@@ -5,8 +5,11 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+
     long long n;
+    long long high = LONG_LONG_MAX, low = 0, middle;
+
     cin >> n;
-    long long res = (long long)ceil(sqrt(n));
+    
     cout << res;
 }
