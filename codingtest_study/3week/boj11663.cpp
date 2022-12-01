33d129e825f728a958174fd467e3f8c7cc4324f8
Jahoon Lee <aoleejohn@naver.com>
2022-12-01 18:19:15 +0900

    1

--- codingtest_study/3week/boj11663.cpp
+++ codingtest_study/3week/boj11663.cpp
@@ -1,23 +1,29 @@
-/* 선분 위의 점 */
+/*선분 위의 점*/
 #include <iostream>
-#include <map>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-	int n,m,num;
-	cin >> n >> m;
-	map<int,int> arr;
-	for(int i=0;i<n;i++)
+    int N, M, start, end;
+	cin >> N >> M;
+    vector<int> v;//자동으로 메모리가 할당되는 배열 이라고 생각하면 된다.
+    for(int i=0; i<N; i++)
 	{
-		cin >> num;
-		arr.insert({num,0});
-	}
-	for(int i=0;i<n;i++)
+        int inp;
+		cin >> inp;
+        v.push_back(inp);
+    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<M; i++)
 	{
-
-	}
-
+		cin >> start >> end;
+        int first = lower_bound(v.begin(), v.end(), start) - v.begin();//start와 같거나 처음으로 큰수
+        int second = upper_bound(v.begin(), v.end(), end) - v.begin();//end보다 처음으로 작은수
+        cout<<second - first<<'\n';
+    }
 }
---
+++ codingtest_study/3week/boj2805.cpp
@@ -0,0 +1,42 @@
+/*나무 자르기*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    long long n, m, res = -1;
+    long long start, end, mid, sum;
+	vector<long long> v;
+    cin >> n >> m;
+
+    for(int i = 0 ; i < n ; i++)
+	{
+		cin >> sum;
+		v.push_back(sum);
+	}
+    sort(v.begin(),v.end());
+
+    start = 0;
+    end = v[n - 1];
+
+    while(start <= end){
+        sum = 0;
+        mid = (start + end) / 2;
+
+        for(int i = 0 ; i < n ; i++)
+            if(v[i] - mid > 0) sum += v[i] - mid; //자른 나무 길이
+        if(sum >= m)
+		{
+            start = mid + 1;
+            if(mid > res) res = mid;
+        }
+        else end = mid - 1;
+    }
+
+    cout << res;
+
+    return 0;
+}
