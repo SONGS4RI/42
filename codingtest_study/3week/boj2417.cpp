09e9500ecebb909a11568d08755a882a64ae5d4a
Jahoon Lee <aoleejohn@naver.com>
2022-12-01 14:15:58 +0900

    1

--- codingtest_study/3week/boj2417.cpp
+++ codingtest_study/3week/boj2417.cpp
@@ -1,3 +1,5 @@
+/* 정수제곱근 */
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -6,14 +8,14 @@ int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	long long n;
 	cin >> n;
-	
-	long long start = 0, end = sqrt(n), mid = 0;
+	long long start = 0, end = n, mid;
 	while (start <= end) {
 		mid = (start + end) / 2;
-		if (mid > sqrt(n)) end = mid - 1;
-		else if(mid < sqrt(n))start = mid + 1;
-		else break;
+		if (mid >= (n / mid)) end = mid - 1;
+		else if(mid < (n / mid)) start = mid + 1;
 	}
 	if (mid * mid == n) cout << mid;
 	else cout << mid + 1;
+	cout.precision(15);
+	cout << "\nsqrt(n) : " << sqrt(n);
 }
