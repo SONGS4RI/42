53756e2cf681d079438ad5c0d60af284d29e26a5
Jahoon Lee <aoleejohn@naver.com>
2022-12-08 20:22:00 +0900

    1

--- codingtest_study/0x03_array/boj10808.cpp
+++ codingtest_study/0x03_array/boj10808.cpp
@@ -1,1 +1,14 @@
 /*알파벳 개수*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int arr[26] = {0,};
+	string s;
+	cin >> s;
+	for(int i=0;i<s.size();i++)
+		arr[s[i]-'a']++;
+	for(int i=0;i<26;i++)
+		cout << arr[i] << " ";
+}
