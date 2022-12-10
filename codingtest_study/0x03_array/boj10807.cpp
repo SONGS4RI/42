087e0cc50a75afaab28e4b17906cfbd80bf53c51
Jahoon Lee <aoleejohn@naver.com>
2022-12-10 15:11:23 +0900

    1

---
+++ codingtest_study/0x03_array/boj10807.cpp
@@ -0,0 +1,17 @@
+/* 개수 세기 */
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int n, num, arr[201]={0,};
+	cin >> n;
+	for(int i=0;i<n;i++)
+	{
+		cin >> num;
+		arr[num+100]++;
+	}
+	cin >> num;
+	cout << arr[num+100] << "\n";
+}
---
+++ codingtest_study/0x03_array/boj13300.cpp
@@ -0,0 +1,30 @@
+/* πÊ πË¡§ */
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int n,k,a,b,ans=0;
+	cin >> n >> k;
+	int arr[7][2];
+	memset(arr,0,sizeof(arr));
+	for(int i=0;i<n;i++)
+	{
+		cin >> b >> a;
+		arr[a][b]++;
+	}
+	for(int i=1;i<=6;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			if(arr[i][j]!=0)
+			{
+				if(arr[i][j]%k == 0) ans += arr[i][j]/k;
+				else ans += arr[i][j]/k + 1;
+			}
+		}
+	}
+	cout << ans << "\n";
+}
