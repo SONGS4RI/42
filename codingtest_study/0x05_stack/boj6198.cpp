ad74422b2a346a619a713d8585325003c5ebe995
Jahoon Lee <jahlee@student.42seoul.kr>
2022-12-19 13:10:06 +0900

    1

---
+++ codingtest_study/0x05_stack/boj6198.cpp
@@ -0,0 +1,26 @@
+/* 탑 */
+/* 12:42 ~ 13:08*/
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int n, num;
+	long long ans=0;//롱롱 주의해야함
+	stack<int> st;
+	cin >> n;
+	for(int i=0;i<n;i++)
+	{
+		cin >> num;
+		while(st.size() && st.top()<=num)
+		{
+			st.pop();
+			ans+=st.size();
+		}
+		st.push(num);
+	}
+	if(st.size()) ans += ((st.size()-1)*st.size())/2;// 1~n-1까지의 합
+	cout << ans << "\n";
+}
