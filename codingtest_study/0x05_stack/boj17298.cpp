40d7a7f91b4a71465afa1e498926e25f74260b2b
Jahoon Lee <jahlee@student.42seoul.kr>
2022-12-19 14:00:29 +0900

    1

---
+++ codingtest_study/0x05_stack/boj17298.cpp
@@ -0,0 +1,27 @@
+/* 오큰수 */
+/* 13:22 ~  13:59*/
+#include <iostream>
+#include <stack>
+#include <string.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int n, num, idx=0;
+	stack<pair<int,int>> st;
+	cin >> n;
+	int ans[n];
+	memset(ans,-1,sizeof(ans));
+	for(int i=0;i<n;i++)
+	{
+		cin >> num;
+		while(st.size() && st.top().first < num)
+		{
+			ans[st.top().second] = num;
+			st.pop();
+		}
+		st.push({num,i});
+	}
+	for(int i=0;i<n;i++) cout << ans[i] << " ";
+}
