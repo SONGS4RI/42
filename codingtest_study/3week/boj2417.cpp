32ab6606cc7be1ab576b87c43a8d04d149109c60
Jahoon Lee <aoleejohn@naver.com>
2022-12-25 16:45:59 +0900

    1

--- codingtest_study/3week/boj2417.cpp
+++ codingtest_study/3week/boj2417.cpp
@@ -4,17 +4,18 @@
 #include <cmath>
 using namespace std;
 
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false); cin.tie(NULL);
+
 	long long n;
 	cin >> n;
-	long long start = 0, end = n, mid;
-	while (start <= end) {
-		mid = (start + end) / 2;
-		if (mid >= (n / mid)) end = mid - 1;
-		else if(mid < (n / mid)) start = mid + 1;
+	long long left=0, right = n, mid;
+	while(left<=right)
+	{
+		mid = (left+right)/2;
+		if(mid*mid < n && mid <= sqrt(__LONG_LONG_MAX__)) left = mid+1;
+		else right = mid -1;
 	}
-	if (start * start < n && start * start > 0) cout << start + 1;
-	else if (end * end == n) cout << end;
-	else cout << end + 1;
+	cout << left;
 }
