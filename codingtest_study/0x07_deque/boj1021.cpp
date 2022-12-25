ebf369f7e8bdbbd4224cb16e02c5815efc918d82
Jahoon Lee <aoleejohn@naver.com>
2022-12-25 16:20:31 +0900

    1

--- codingtest_study/0x07_deque/boj1021.cpp
+++ codingtest_study/0x07_deque/boj1021.cpp
@@ -8,27 +8,22 @@ int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-	int n, m, num, ans=0;;
-	deque<int> dq, arr;
+	int n, m, num, ans=0;
+	deque<int> dq;
 	cin >> n >> m;
 	for(int i=1;i<=n;i++) dq.push_back(i);
 	for(int i=0;i<m;i++)
 	{
 		cin >> num;
-		arr.push_back(num);
-	}
-	while(arr.size())//탐색수가 다 떨어질때 까지
-	{
-		num = 0;
-		while(arr.front()!=dq.front())//탐색수와 순환큐의 front값이 같을때 까지 2번(왼쪽으로 돌리는 형식)적용
+		int tmp = 0;
+		while(num!=dq.front())//탐색수와 순환큐의 front값이 같을때 까지 2번(왼쪽으로 돌리는 형식)적용
 		{
 			dq.push_back(dq.front());
 			dq.pop_front();
-			num++;
+			tmp++;
 		}
-		ans += min(num,(int)dq.size()-num);//오른쪽 왼쪽 중 작은것 선택
-		dq.pop_front();
-		arr.pop_front();
+		ans += min(tmp,(int)dq.size()-tmp);//왼쪽 오른쪽 돌리는 방식 중 작은것 선택
+		dq.pop_front();					   //왼쪽 == tmp 오른쪽 dq.size()-tmp
 	}
 	cout << ans << "\n";
 }
