fc38841703f2e77c1dffc34aa24713caeaadc6e3
Jahlee <jahlee@student.42.fr>
2023-01-02 19:10:14 +0900

    1

--- codingtest_study/0x09_bfs/boj1012.cpp
+++ codingtest_study/0x09_bfs/boj1012.cpp
@@ -10,18 +10,17 @@ int main()
 
 	int t,m,n,k,nx,ny;
 	int dx[4] = {-1,0,1,0}, dy[4] = {0,1,0,-1};
-	int board[51][51];
 	cin >> t;
 	while(t--)
 	{
 		cin >> m >> n >> k;
 		int ans = 0;
-		memset(board,0,sizeof(board));
+		int board[n][m]; memset(board,0,sizeof(board));//0으로 초기화
 		queue<pair<int,int> > q;
 		for(int i=0;i<k;i++)
 		{
 			cin >> nx >> ny;
-			board[ny][nx] = 1;
+			board[ny][nx] = 1;// 배추표기
 		}
 		for(int i=0;i<n;i++)
 		{
@@ -30,7 +29,7 @@ int main()
 				if(board[i][j]==1)
 				{
 					q.push(make_pair(i,j));
-					board[i][j] = 2;
+					board[i][j] = 2;//방문한 배추이면 2로 표기
 					while(q.size())
 					{
 						pair<int,int> cur = q.front();
@@ -40,7 +39,7 @@ int main()
 							nx = cur.first + dx[dir];
 							ny = cur.second + dy[dir];
 							if(nx<0 || ny<0 || nx>=n || ny>= m) continue;
-							if(board[nx][ny]!=1) continue;
+							if(board[nx][ny]!=1) continue;//1일떄만 방문안한 배추이기떄문
 							q.push(make_pair(nx,ny));
 							board[nx][ny] = 2;
 						}
