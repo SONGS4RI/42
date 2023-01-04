b6f15090b3496d35b4a626731b5fcbd4211c83e5
Jahlee <jahlee@student.42.fr>
2023-01-04 20:19:55 +0900

    1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,19 +6,59 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:29 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/04 14:00:24 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/04 20:19:19 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>///////
+
+// t_gnl_list	*find_fd(t_gnl_list *head, int fd)
+// {
+// 	while (head->next)
+// 	{
+// 		if (head->next->fd_idx == fd)
+// 			break ;
+// 		head = head->next;
+// 	}
+// 	if (!head)
+// 		return (new_fd(head, fd)->next);
+// 	else
+// 		return (head->next);
+// }
+
+// t_gnl_list	*new_fd(t_gnl_list *head, int fd)
+// {
+// 	t_gnl_list	*new;
+
+// 	head->next = new;
+// 	new->fd_idx = fd;
+// 	new->next = NULL;
+// 	return (new);
+// }
 
 char	*get_next_line(int fd)
 {
-	char		buf[BUFFER_SIZE];
-	static char	*backup;
+	static t_gnl_list	*head;
+	t_gnl_list			*buf;
+	char				*next_line;
 
 	if (fd < 0)
 		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
-	return (NULL);
+	// fd값이랑 일치하는 리스트가 있는지 확인
+	//find_fd(head,fd);
+	
+	return (next_line);
+}
+////////////////////////////////////////////////////////////////
+#include <fcntl.h>
+int main()
+{
+	static t_gnl_list	*head;
+	t_gnl_list *tmp;
+	int fd;
+
+	tmp = head;
+	// fd = open("./txt1.txt",O_RDONLY);
+	if(!head->fd_idx) printf("NULL\n");
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,68 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:27 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/03 19:14:32 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/04 19:30:37 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	cnt;
+
+	cnt = 0;
+	while (s[cnt])
+		cnt++;
+	return (cnt);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i + 1 < dstsize && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	new_len;
+
+	if (!s)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	new_len = ft_strlen(s + start);
+	if (new_len < len)
+		len = new_len;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	size;
+	char	*mem;
+
+	size = ft_strlen(s);
+	mem = (char *)malloc(sizeof(char) * (size + 1));
+	if (!mem)
+		return (NULL);
+	ft_strlcpy(mem, s, size + 1);
+	return (mem);
+}
