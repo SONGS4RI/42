aaf1f8094800bdeb0386555b09f65656f1cdf303
Jahlee <jahlee@student.42.fr>
2023-01-04 14:00:25 +0900

    1

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 19:14:29 by jahlee            #+#    #+#             */
+/*   Updated: 2023/01/04 14:00:24 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE];
+	static char	*backup;
+
+	if (fd < 0)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	return (NULL);
+}
