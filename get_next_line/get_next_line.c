f4efe5823d4e8da7e9a0443ec45dd694ad009693
Jahlee <jahlee@student.42.fr>
2023-01-05 20:59:31 +0900

    1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,36 +6,38 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:29 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/04 20:19:19 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/05 20:59:14 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>///////
 
-// t_gnl_list	*find_fd(t_gnl_list *head, int fd)
-// {
-// 	while (head->next)
-// 	{
-// 		if (head->next->fd_idx == fd)
-// 			break ;
-// 		head = head->next;
-// 	}
-// 	if (!head)
-// 		return (new_fd(head, fd)->next);
-// 	else
-// 		return (head->next);
-// }
+t_gnl_list	*find_fd(t_gnl_list *tmp, int fd)
+{
+	t_gnl_list	*new;
 
-// t_gnl_list	*new_fd(t_gnl_list *head, int fd)
-// {
-// 	t_gnl_list	*new;
+	while (tmp->next)
+	{
+		if (tmp->fd_idx == fd)
+			break ;
+		tmp = tmp->next;
+	}
+	if (tmp->next)
+		return (tmp);
+	else
+	{
+		tmp->next = new_fd(new, fd);
+	}
+}
 
-// 	head->next = new;
-// 	new->fd_idx = fd;
-// 	new->next = NULL;
-// 	return (new);
-// }
+t_gnl_list	*new_fd(t_gnl_list *tmp, int fd)
+{
+	tmp = (t_gnl_list *)malloc(sizeof(t_gnl_list));
+	tmp->fd_idx = fd;
+	tmp->next = NULL;
+	return (tmp);
+}
 
 char	*get_next_line(int fd)
 {
@@ -45,10 +47,11 @@ char	*get_next_line(int fd)
 
 	if (fd < 0)
 		return (NULL);
-	// fd값이랑 일치하는 리스트가 있는지 확인
-	//find_fd(head,fd);
-	
-	return (next_line);
+	if (!head)
+		head = new_fd(head, fd);
+	else
+		buf = find_fd(head, fd);
+	return ("good");
 }
 ////////////////////////////////////////////////////////////////
 #include <fcntl.h>
@@ -58,7 +61,6 @@ int main()
 	t_gnl_list *tmp;
 	int fd;
 
-	tmp = head;
-	// fd = open("./txt1.txt",O_RDONLY);
-	if(!head->fd_idx) printf("NULL\n");
+	fd = open("./txt1.txt",O_RDONLY);
+	get_next_line(fd);
 }
