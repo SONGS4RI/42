27dfd313016cf51bb4455368c14e229e3f4bbfd7
Jahlee <jahlee@student.42.fr>
2023-01-10 19:24:06 +0900

    1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,48 +6,44 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:29 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/10 18:22:20 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/10 19:23:31 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+// #include <stdio.h>//////////////////
 t_gnl_list	*del_gnl_list(t_gnl_list *tmp)
 {
 	t_gnl_list	*previous;
 
-	previous = tmp->previous;
-	previous->next = tmp->next;
-	if (tmp->backup)
-		free(tmp->backup);
+	if (tmp->previous)
+	{
+		previous = tmp->previous;
+		previous->next = tmp->next;
+	}
 	free(tmp);
 	return (NULL);
 }
 
 t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, int cnt)
 {
-	t_gnl_list	*new;
-
-	new = tmp;
 	while (tmp)
 	{
 		if (tmp->fd_idx == fd)
 			break ;
-		new = tmp;
 		tmp = tmp->next;
 	}
 	if (tmp)
 		return (tmp);
-	else if (cnt)
+	else if (cnt > 0)
 	{
 		tmp = (t_gnl_list *)malloc(sizeof(t_gnl_list));
 		if (!tmp)
 			return (NULL);
-		tmp->previous = new;
+		if (tmp->previous)
+			tmp->previous->next = tmp;
 		tmp->fd_idx = fd;
 		tmp->next = NULL;
-		if (new)
-			new->next = tmp;
 		return (tmp);
 	}
 	return (NULL);
@@ -60,9 +56,9 @@ char	*combine_buf_read(int len_s, char *s, int idx, t_gnl_list *buf)
 
 	len = len_s + idx;
 	tmp = (char *)malloc(len + 1);
-	tmp[0] = '\0';
 	if (!tmp)
 		return (NULL);
+	tmp[0] = '\0';
 	if (buf->backup)
 	{
 		ft_strlcat(tmp, buf->backup, idx + 1);
@@ -114,6 +110,8 @@ char	*get_next_line(int fd)
 	if (!head)
 		head = find_fd(head, fd, cnt);
 	buf = find_fd(head, fd, cnt);
+	if (!buf)
+		return (NULL);
 	if (!cnt && !buf->backup)
 	{
 		del_gnl_list(buf);
@@ -123,3 +121,24 @@ char	*get_next_line(int fd)
 		buf_size = ft_strlen(buf->backup);
 	return (combine_buf_read(cnt, next_line, buf_size, buf));
 }
+
+// #include <fcntl.h>
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int fd;
+// 	fd = open("files/nl",O_RDONLY);
+// 	printf("--------------------\n");
+// 	printf("%s",get_next_line(fd));
+// 	printf("%s",get_next_line(fd));
+// 	// printf("|%s|\n",get_next_line(fd));
+// 	// printf("|%s|\n",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+// 	// get_next_line(fd);
+// 	// fd = open("./txt2.txt",O_RDONLY);
+// 	// get_next_line(fd);
+// 	// fd = open("./txt3.txt",O_RDONLY);
+// 	// get_next_line(fd);
+
+// }
