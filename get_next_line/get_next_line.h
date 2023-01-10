d292830fddacc38cdb212188c07ccc1827a2fd0b
Jahlee <jahlee@student.42.fr>
2023-01-10 17:47:29 +0900

    1

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:16:28 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/10 17:03:35 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/10 17:32:38 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -24,12 +24,14 @@ typedef struct s_gnl_list
 	int					fd_idx;
 	char				*backup;
 	struct s_gnl_list	*next;
+	struct s_gnl_list	*previous;
 }t_gnl_list;
 
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
+t_gnl_list	*del_gnl_list(t_gnl_list *tmp);
 t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, int cnt);
 char		*is_backup(int len_s, char *s, int len_buf, t_gnl_list *buf);
 char		*get_next_line(int fd);
