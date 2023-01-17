7c594a72e3efd34d4948157e8ffd8f6fcf30ff51
Jahlee <jahlee@student.42.fr>
2023-01-17 20:20:47 +0900

    1

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:16:28 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/15 20:37:35 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/17 20:02:05 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ size_t		ft_strlcat(char *dst, const char *s, size_t s_len, size_t dstsize);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 int			is_nl(char *tmp, int read_cnt, t_gnl_list *head);
-t_gnl_list	*del_gnl_list(t_gnl_list *tmp, int read_cnt);
+t_gnl_list	*del_gnl_list(t_gnl_list *tmp);
 t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, int read_cnt);
 char		*combine_all(t_gnl_list	*tmp, char *next_line, int read_cnt);
 char		*res_line(t_gnl_list	*head, char *tmp, int idx);
