42b5fc7275ff07f6e553105c3b9aabe86bcf72f1
Jahlee <jahlee@student.42.fr>
2023-01-18 21:51:15 +0900

    1

--- get_next_line copy/get_next_line.h
+++ get_next_line copy/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:16:28 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/17 20:02:05 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/18 21:24:34 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,19 @@
 typedef struct s_gnl_list
 {
 	int					fd_idx;
-	int					eof;
 	struct s_gnl_list	*next;
 	struct s_gnl_list	*previous;
 	char				*backup;
 }t_gnl_list;
 
 size_t		ft_strlen(const char *s);
-size_t		ft_strlcat(char *dst, const char *s, size_t s_len, size_t dstsize);
-size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize, size_t idx);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
-int			is_nl(char *tmp, int read_cnt, t_gnl_list *head);
+char		*combine_all(char *str1, char *str2, int len_1, int len_2);
+char		*is_nl(t_gnl_list *tmp, char *line, int read_cnt);
 t_gnl_list	*del_gnl_list(t_gnl_list *tmp);
-t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, int read_cnt);
-char		*combine_all(t_gnl_list	*tmp, char *next_line, int read_cnt);
-char		*res_line(t_gnl_list	*head, char *tmp, int idx);
+t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, int read_cnt, t_gnl_list	*head);
+char		*is_nl_backup(char *str, int len, int read_cnt, t_gnl_list *tmp);
+char		*is_nl_line(char *str, int len, t_gnl_list *tmp);
 char		*get_next_line(int fd);
 #endif
--- get_next_line copy/get_next_line_utils.c
+++ get_next_line copy/get_next_line_utils.c
@@ -6,57 +6,40 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:27 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/17 20:20:40 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/18 21:50:35 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>///////////////////////////
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	cnt;
 
+	if (!s)
+		return (0);
 	cnt = 0;
 	while (s[cnt])
 		cnt++;
 	return (cnt);
 }
 
-size_t	ft_strlcat(char *dst, const char *s, size_t s_len, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize, size_t idx)
 {
-	size_t	i;
-	size_t	dst_len;
-
-	i = 0;
-	dst_len = ft_strlen(dst);
-	if (dstsize < dst_len + 1)
-		return (dstsize + s_len);
-	if (dstsize > dst_len + 1)
-	{
-		while (dstsize > dst_len + 1 + i && i < s_len)
-		{
-			dst[dst_len + i] = s[i];
-			i++;
-		}
-	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + s_len);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
+	int	i;
 
 	i = 0;
 	if (dstsize == 0)
 		return (ft_strlen(src));
-	while (i + 1 < dstsize && src[i])
+	while (idx + 1 < dstsize && src[i])
 	{
-		dst[i] = src[i];
+		dst[idx] = src[i];
+		idx++;
 		i++;
 	}
-	dst[i] = '\0';
-	return (i);
+	dst[idx] = '\0';
+	return (idx);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -75,31 +58,52 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!substr)
 		return (NULL);
 	substr[0] = '\0';
-	ft_strlcpy(substr, s + start, len + 1);
+	ft_strlcpy(substr, s + start, len + 1, 0);
+	if (substr[0] == '\0')
+	{
+		free(substr);
+		return (NULL);
+	}
 	return (substr);
 }
 
-int	is_nl(char *tmp, int read_cnt, t_gnl_list *head)
+char	*combine_all(char *str1, char *str2, int len_1, int len_2)
 {
-	int	idx;
+	char	*res;
 
-	idx = 0;
-	if (!head || read_cnt == -1 || !tmp)
-		return (-1);
-	while (tmp[idx])
+	res = (char *)malloc(sizeof(char) * (len_1 + len_2 + 1));
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, str1, len_1 + 1, 0);
+	ft_strlcpy(res, str2, len_1 + len_2 + 1, len_1);
+	if (str1)
 	{
-		if (tmp[idx] == '\n')
-			return (idx + 1);
-		idx++;
+		free(str1);
+		printf("freed str1 at %p\n",str1);/////////////////////////
 	}
+	if (str2)
+	{
+		free(str2);
+		printf("freed str2 at %p\n",str2);/////////////////////////
+	}
+	return (res);
+}
+
+char	*is_nl(t_gnl_list *tmp, char *line, int read_cnt)
+{
+	char	*res;
+
+	res = NULL;
+	if (tmp->backup)
+		res = is_nl_backup(line, ft_strlen(tmp->backup), read_cnt, tmp);
+	if (!res)
+		res = is_nl_line(line, read_cnt, tmp);
 	if (read_cnt < BUFFER_SIZE)
 	{
-		head->eof = 1;
-		return (idx + 1);
+		res = ft_substr(tmp->backup, 0, ft_strlen(tmp->backup));
+		printf("(is_nl)res malloced at %p\n",res);////////////////////////////
+		free(line);
+		printf("(is_nl)freed line at %p\n",line);/////////////////////////
 	}
-	head->backup = ft_substr(tmp, 0, idx + 1);
-	free(tmp);
-	if (!head->backup)
-		return (-1);
-	return (0);
+	return (res);
 }
