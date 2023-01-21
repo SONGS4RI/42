e939e0897aead6ee7b93d42a9ab8b95c72e45adf
Jahlee <jahlee@student.42.fr>
2023-01-21 21:24:40 +0900

    1

--- gnl_last/get_next_line.c
+++ gnl_last/get_next_line.c
@@ -1,39 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:29 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/21 13:23:54 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/21 21:23:11 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>/////////////////////
 
-t_gnl_list	*del_gnl_list(t_gnl_list *tmp)
+t_gnl_list	*del_gnl_list(t_gnl_list **tmp)
 {
 	t_gnl_list	*head;
 
 	head = NULL;
-	if (tmp->previous)
+	if ((*tmp)->previous)
 	{
-		tmp->previous->next = tmp->next;
-		if (tmp->next)
-			tmp->next->previous = tmp->previous;
+		(*tmp)->previous->next = (*tmp)->next;
+		if ((*tmp)->next)
+			(*tmp)->next->previous = (*tmp)->previous;
+		head = (*tmp);
 		while (head)
 			head = head->previous;
 	}
-	else if (tmp->next)
+	else if ((*tmp)->next)
 	{
-		tmp->next->previous = NULL;
-		head = tmp->next;
+		(*tmp)->next->previous = NULL;
+		head = (*tmp)->next;
 	}
-	free(tmp->backup);
-	free(tmp);
-	// printf("list freed at %p\n",tmp);////////////////////////
+	if ((*tmp)->backup)
+		free((*tmp)->backup);
+	if ((*tmp)->backup)
+		(*tmp)->backup = NULL;
+	free((*tmp));
+	(*tmp) = NULL;
 	return (head);
 }
 
@@ -51,11 +55,11 @@ t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, t_gnl_list *head)
 		tmp = (t_gnl_list *)malloc(sizeof(t_gnl_list));
 		if (!tmp)
 			return (NULL);
-		// printf("list malloced at %p\n",tmp);////////////////////////
 		tmp->previous = NULL;
 		tmp->next = NULL;
 		tmp->backup = NULL;
 		tmp->fd_idx = fd;
+		tmp->eof = 0;
 		if (head)
 		{
 			head->next = tmp;
@@ -65,56 +69,57 @@ t_gnl_list	*find_fd(t_gnl_list *tmp, int fd, t_gnl_list *head)
 	return (tmp);
 }
 
-char	*combine_all(char *str1, char *str2)
+char	*combine_all(char **str1, char **str2, t_gnl_list **tmp)
 {
 	char	*res;
 	int		len_1;
 	int		len_2;
 
-	len_1 = ft_strlen(str1);
-	len_2 = ft_strlen(str2);
+	len_1 = ft_strlen(*str1);
+	len_2 = ft_strlen(*str2);
 	res = (char *)malloc(sizeof(char) * (len_1 + len_2 + 1));
-	if (!res)
+	if (!res)//NULL check
+	{
+		(*tmp)->eof = 1;
 		return (NULL);
-	ft_strlcpy(res, str1, len_1 + 1, 0);
-	ft_strlcpy(res, str2, len_1 + len_2 + 1, len_1);
-	if (str1)
-	{
-		free(str1);
-		// printf("freed str1 at %p\n",str1);/////////////////////////
 	}
-	if (str2)
+	ft_strlcpy(res, *str1, len_1 + 1, 0);
+	ft_strlcpy(res, *str2, len_1 + len_2 + 1, len_1);
+	if (*str1)
 	{
-		free(str2);
-		// printf("freed str2 at %p\n",str2);/////////////////////////
+		free(*str1);
+		*str1 = NULL;
+	}
+	if (*str2)
+	{
+		free(*str2);
+		*str2 = NULL;
 	}
 	return (res);
 }
 
-char	*read_line(t_gnl_list *tmp, t_gnl_list **pt, int fd)
+char	*read_line(t_gnl_list **tmp, int fd)
 {
 	char	*line;
 	char	*res;
 	int		read_cnt;
 
-	line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!line)
+	line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!line)//NULL check
 		return (NULL);
 	read_cnt = read(fd, line, BUFFER_SIZE);
-	if ((read_cnt <=0 && !tmp) || read_cnt == -1)
+	if ((read_cnt == 0 && !(*tmp)->backup) || read_cnt == -1)
 	{
 		free(line);
-		if (read_cnt == -1)// read err 처리
-			del_gnl_list(tmp);//
+		(*tmp)->eof = 1;
 		return (NULL);
 	}
 	line[read_cnt] = '\0';
-	res = is_nl_line(&line, read_cnt, *pt);
-	if (read_cnt < BUFFER_SIZE && !res)
+	res = is_nl_line(&line, read_cnt, tmp);
+	if (read_cnt < BUFFER_SIZE && !res && (*tmp)->backup && !(*tmp)->eof)
 	{
-		res = ft_substr(tmp->backup, 0, ft_strlen(tmp->backup));
-		del_gnl_list(tmp);
-		*pt = NULL;
+		res = ft_substr((*tmp)->backup, 0, ft_strlen((*tmp)->backup), tmp);
+		(*tmp)->eof = 1;
 	}
 	return (res);
 }
@@ -138,29 +143,36 @@ char	*get_next_line(int fd)
 	}
 	while (1)
 	{
-		res = read_line(tmp, &tmp, fd);
-		if (res || !tmp)
+		res = read_line(&tmp, fd);
+		if (tmp->eof || res)
+		{
+			if (tmp->eof)
+				head = del_gnl_list(&tmp);
 			return (res);
+		}
 	}
-	if (!tmp)
-		head = NULL;
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-int main()
-{
-	int fd[3];
-	fd[0] = open("./files/1",O_RDONLY);
-	fd[1] = open("./files/2",O_RDONLY);
-	fd[2] = open("./files/3",O_RDONLY);
-	printf("|%s|\n",get_next_line(fd[0]));
-	printf("|%s|\n",get_next_line(fd[1]));
-	printf("|%s|\n",get_next_line(fd[2]));
-	printf("|%s|\n",get_next_line(fd[0]));
-	printf("|%s|\n",get_next_line(fd[1]));
-	printf("|%s|\n",get_next_line(fd[2]));
-	printf("|%s|\n",get_next_line(fd[0]));
-	printf("|%s|\n",get_next_line(fd[1]));
-	printf("|%s|\n",get_next_line(fd[2]));
-}
+// #include <fcntl.h>
+// #include <stdio.h>
+// int main()
+// {
+// 	int fd[3];
+// 	fd[0] = open("./files/41_no_nl",O_RDONLY);
+// 	// fd[1] = open("./files/2",O_RDONLY);
+// 	// fd[2] = open("./files/3",O_RDONLY);
+// 	printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[1]));
+// 	// printf("|%s|\n",get_next_line(fd[2]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[1]));
+// 	// printf("|%s|\n",get_next_line(fd[2]));
+// 	// printf("|%s|\n",get_next_line(fd[0]));
+// 	// printf("|%s|\n",get_next_line(fd[1]));
+// 	// printf("|%s|\n",get_next_line(fd[2]));
+// }
--- gnl_last/get_next_line_utils.c
+++ gnl_last/get_next_line_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:14:27 by jahlee            #+#    #+#             */
-/*   Updated: 2023/01/21 13:24:09 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/01/21 21:24:04 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,23 +41,32 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize, size_t idx)
 	return (idx);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char *s, int start, size_t len, t_gnl_list **tmp)
 {
 	char	*substr;
 	size_t	new_len;
 
 	if (!s || s[0] == '\0')
 		return (NULL);
-	if ((unsigned int)ft_strlen(s) < start)
+	if ((int)ft_strlen(s) < start)
 		return (NULL);
 	new_len = ft_strlen(s + start);
 	if (new_len < len)
 		len = new_len;
 	substr = (char *)malloc(sizeof(char) * (len + 1));
-	if (!substr)
+	if (!substr)//NULL check
+	{
+		free(s);
+		(*tmp)->eof = 1;///
 		return (NULL);
+	}
 	substr[0] = '\0';
 	ft_strlcpy(substr, s + start, len + 1, 0);
+	if (substr[0] == '\0')
+	{
+		free(substr);
+		return (NULL);
+	}
 	return (substr);
 }
 
@@ -77,21 +86,24 @@ char	*is_nl_backup(char **str, int len, t_gnl_list *tmp)
 	}
 	if (idx < len)
 	{
-		res = ft_substr(*str, 0, idx + 1);
-		backup = ft_substr(*str, idx + 1, len);
+		res = ft_substr(*str, 0, idx + 1, &tmp);
+		backup = ft_substr(*str, idx + 1, len, &tmp);
+		if (tmp->eof)
+			return (NULL);
 		free(tmp->backup);
 		tmp->backup = backup;
 	}
 	return (res);
 }
 
-char	*is_nl_line(char **str, int len, t_gnl_list *tmp)
+char	*is_nl_line(char **str, int len, t_gnl_list **tmp)
 {
 	int		idx;
 	char	*res;
 	char	*backup;
 
 	res = NULL;
+	backup = NULL;
 	idx = 0;
 	while ((*str)[idx])
 	{
@@ -101,11 +113,15 @@ char	*is_nl_line(char **str, int len, t_gnl_list *tmp)
 	}
 	if (idx < len)
 	{
-		res = combine_all(tmp->backup, ft_substr(*str, 0, idx + 1));
-		backup = ft_substr(*str, idx + 1, len);
+		res = ft_substr(*str, 0, idx + 1, tmp);
+		res = combine_all(&(*tmp)->backup, &res, tmp);
+		backup = ft_substr(*str, idx + 1, len, tmp);
+		free(*str);
 	}
 	else
-		backup = combine_all(tmp->backup, ft_substr(*str, 0, len));
-	tmp->backup = backup;
+		backup = combine_all(&(*tmp)->backup, str, tmp);
+	if ((*tmp)->eof)
+		return (NULL);
+	(*tmp)->backup = backup;
 	return (res);
 }
