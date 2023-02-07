36c85e67029ce7afb2e22d9dfe3193ba384c4a17
Jahlee <jahlee@student.42.fr>
2023-02-07 21:06:41 +0900

    1

---
+++ push_swap/libft/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 11:22:21 by jahlee            #+#    #+#             */
+/*   Updated: 2022/11/24 19:56:41 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t'
+		|| c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	int		nbr;
+	size_t	i;
+
+	sign = 1;
+	nbr = 0;
+	i = 0;
+	while (str[i] && ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if ((str[i] == '-') || (str[i] == '+'))
+		i++;
+	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+	{
+		nbr = (nbr * 10) + sign * (str[i] - '0');
+		i++;
+	}
+	return (nbr);
+}
