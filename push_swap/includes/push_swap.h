2ad3490eae6d4c630ebf8f69ca1d141660f01cf1
Jahoon Lee <aoleejohn@naver.com>
2023-02-09 19:27:02 +0900

    1

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 19:50:02 by jahlee            #+#    #+#             */
-/*   Updated: 2023/02/07 21:01:25 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/02/09 18:44:59 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+
+typedef struct s_stack
+{
+	int		num;
+	s_stack *next;
+	s_stack *prevoius;
+	s_stack *top;
+	s_stack *bottom;
+}t_stack;
+
 
 int	*init_a(char **argv);
 
