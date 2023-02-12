971c0e17b267b029f5bc343862e87190249f7732
Jahlee <jahlee@student.42.fr>
2023-02-12 19:01:08 +0900

    1

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 19:50:02 by jahlee            #+#    #+#             */
-/*   Updated: 2023/02/12 17:49:56 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/02/12 19:01:02 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ typedef struct s_stack
 	struct s_stack_node	*bottom;
 }t_stack;
 
+void			leaks(void);/////////////
+
 void			argv_to_line(char **argv, t_stack **st);
 void			init_stack(t_stack **st_a, t_stack **st_b);
 
@@ -37,7 +39,7 @@ void			free_ps_stack(t_stack *st);
 void			err_exit(t_stack *st, char *line, char *err_msg);
 
 static int		is_whitespace(char c);
-static int		ps_atoi(char *str, int offset);
+static int		ps_atoi(char *str, int offset, char *s, t_stack **st);
 static void		add_to_stack(int n, char *s, t_stack **st);
 void			split_to_stack(char *s, t_stack **st);
 
