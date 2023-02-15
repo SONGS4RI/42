ee0f7c227c5fc24ed796b661c9701655de5b4704
jahlee <jahlee@student.42.fr>
2023-02-15 21:02:52 +0900

    1

--- push_swap/srcs/ps_algorithm.c
+++ push_swap/srcs/ps_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 15:48:38 by jahlee            #+#    #+#             */
-/*   Updated: 2023/02/15 20:33:39 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/02/15 21:02:03 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,39 @@ int	compare_num(int a, int b)
 	return (-1);
 }
 
-int	is_sorted(t_stack *st)
+int	is_sorted(t_stack *st, int sorted)
 {
 	t_stack_node 	*tmp;
-	static int		sorted = 1;
+	int				compare;
 
 	if (st->size > 1)
 	{
 		tmp = st->top;
-		sorted = compare_num(tmp->num, tmp->next->num);
-		while (tmp->next && sorted)
+		compare = compare_num(tmp->num, tmp->next->num);
+		if (compare != sorted)
+			return (0);
+		while (tmp->next)
 		{
 			if (sorted != compare_num(tmp->num, tmp->next->num))
-				sorted = 0;
+				return (0);
 			tmp = tmp->next;
 		}
 	}
 	return (sorted);
+}
+
+t_stack	*choose_pivot_stack(t_stack *st_a, t_stack *st_b)
+{
+	int	flag_a;
+	int	flag_b;
+
+	flag_a = is_sorted(st_a, 1);
+	flag_b = is_sorted(st_b, -1);
+	if (!flag_a && !flag_b)
+		return (st_a);
+	else if (!flag_a && flag_b == -1)
+		return (st_a);
+	return (st_b);
 }
 
 void	check_switch_st_top(t_stack *st_a, t_stack *st_b, int pivot)
@@ -57,7 +73,25 @@ void	check_switch_st_top(t_stack *st_a, t_stack *st_b, int pivot)
 		command_s("b", st_a, st_b);
 }
 
-void	check_stacks(t_stack *st_a, t_stack *st_b)
+void	push_swap(t_stack *st_a, t_stack *st_b)
 {
-	
+	t_stack	*pivot_st;
+
+	while (1)
+	{
+		pivot_st = choose_pivot_stack(st_a, st_b);
+		while (1)
+		{
+			
+		}
+		if (st_a->sort == 1 && st_b->sort == -1)
+		{
+			while (st_b->size)
+			{
+				command_p('a', st_a, st_b);
+				check_switch_st_top(st_a, st_b, st_a->bottom->num);
+			}
+			break ;
+		}
+	}
 }
