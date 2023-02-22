7689bc94844775c2b96bae7a65911be9aa3e4a39
Jahlee <jahlee@student.42.fr>
2023-02-22 19:49:55 +0900

    1

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 19:50:02 by jahlee            #+#    #+#             */
-/*   Updated: 2023/02/22 19:34:11 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/02/22 19:46:31 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ int				is_sorted(t_stack *st, int size, int sorted);
 void			free_ps_stack(t_stack *st);
 void			err_exit(t_stack *st, char *line);
 
-void			argv_to_stack(char **argv, t_stack **st);
+void			argv_to_stack(char **argv, t_stack *st);
 void			init_stack(t_stack **st_a, t_stack **st_b);
 void			invalid_check(t_stack *st, t_stack_node	*node, int sorted);
 
 static int		is_whitespace(char c);
-static int		ps_atoi(char *str, int offset, char *s, t_stack **st);
-static void		add_to_stack(int n, char *s, t_stack **st);
-void			split_to_stack(char *s, t_stack **st);
+static int		ps_atoi(char *str, int offset, char *s, t_stack *st);
+static void		add_to_stack(int n, char *s, t_stack *st);
+void			split_to_stack(char *s, t_stack *st);
 
 void			command_s(char c, t_stack *st_a, t_stack *st_b);
 void			command_p(char c, t_stack *st_a, t_stack *st_b);
