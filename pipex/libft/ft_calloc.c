8f8754f722fe9188eeafe603c5bd16c48998a52b
Jahlee <jahlee@student.42.fr>
2023-03-01 14:46:44 +0900

    1

---
+++ pipex/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 16:56:02 by jahlee            #+#    #+#             */
+/*   Updated: 2022/11/21 13:19:16 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*mem;
+
+	mem = malloc(nmemb * size);
+	if (!mem)
+		return (NULL);
+	ft_bzero(mem, (nmemb * size));
+	return (mem);
+}
