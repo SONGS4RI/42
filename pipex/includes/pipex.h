933de63bc78868a8536e092de240c04172a59a02
Jahlee <jahlee@student.42.fr>
2023-03-01 19:24:09 +0900

    1

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,13 +6,14 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:47:38 by jahlee            #+#    #+#             */
-/*   Updated: 2023/03/01 14:48:15 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/03/01 14:52:46 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/libft.h";
-
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <string.h>//perror, strerror
 #endif
