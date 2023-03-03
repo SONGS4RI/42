4540070ad59f389b81621e84c3ecdec1e3ec2844
Jahlee <jahlee@student.42.fr>
2023-03-03 20:15:24 +0900

    1

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,18 +6,20 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:47:38 by jahlee            #+#    #+#             */
-/*   Updated: 2023/03/03 19:58:01 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/03/03 20:08:56 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/libft.h" // unistd.h stdlib.h
+# include "../libft/libft.h"
 # include <fcntl.h>//open, close
 # include <string.h>//perror, strerror
 # include <string.h>//strerror
 # include <stdio.h>//perror
 # include <sys/wait.h>// wait, waitpid
+
+void	exit_err(char *str);
 
 #endif
--- pipex/srcs/px_main.c
+++ pipex/srcs/px_main.c
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 19:38:16 by jahlee            #+#    #+#             */
-/*   Updated: 2023/03/03 19:59:56 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/03/03 20:12:16 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	exit_err(char *str)
 {
-	write(2, str, );
+	write(2, str, ft_strlen(str));
 	exit(1);
 }
 
 int	main(int argc, char **envp)
 {
 	if (argc != 5)
-		return (exit_err("Wrong Usage: ./pipex file1 cmd1 cmd2 file2\n"));
+		exit_err("Wrong Usage: ./pipex file1 cmd1 cmd2 file2\n");
 	return (0);
 }
