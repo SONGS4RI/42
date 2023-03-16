da473047755a7e134633fa46cfb49da265eb38f2
Jahlee <jahlee@student.42.fr>
2023-03-16 19:40:25 +0900

    1

--- pipex/includes/pipex_bonus.h
+++ pipex/includes/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:47:38 by jahlee            #+#    #+#             */
-/*   Updated: 2023/03/15 20:05:14 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/03/16 19:27:28 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,22 @@ typedef struct s_arg
 	pid_t	pid;
 }	t_arg;
 
-void	child_work(t_arg *arg, int i);
-void	parent_work(t_arg *arg);
+void	execute_cmd(t_arg *arg, int idx);
+void	child_work(t_arg *arg, int idx);
+void	parent_work(t_arg *arg, int idx);
+void	pipe_work(t_arg *arg, int idx);
 
 char	**get_path_envp(char **envp);
 char	*get_cmd_argv(char **path, char *cmd);
+void	set_cmd(t_arg *arg, int j);
 void	parse_to_arg(t_arg *arg);
-
-void	free_arg(char **s);
-void	exit_err(t_arg *arg, char *str, char *err_msg, int status);
-void	init_arg(t_arg *arg, int argc, char **argv, char **envp);
 
 void	heredoc(t_arg *arg, char *s, char *tag);
 void	set_infile_fd(t_arg *arg);
 void	set_outfile_fd(t_arg *arg);
+
+void	free_arg(char **s);
+void	exit_err(t_arg *arg, char *str, int status);
+void	init_arg(t_arg *arg, int argc, char **argv, char **envp);
 
 #endif
