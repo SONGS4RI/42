0f5fa6510a5d494fc58b3e1c111b13edbf8e81b9
Jahlee <jahlee@student.42.fr>
2023-06-20 19:28:01 +0900

    ex00 done

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 16:55:25 by jahlee            #+#    #+#             */
+/*   Updated: 2023/06/20 19:25:57 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void makeDefaultMegapehonSound() {
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
+
+void argvToUpper(int argc, char ***argv){
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; (*argv)[i][j] != '\0'; j++) {
+			if (islower((*argv)[i][j])) {
+				(*argv)[i][j] = toupper((*argv)[i][j]);
+			}
+		}
+	}
+}
+
+void makeMegaphoneSound(int argc, char **argv) {
+	argvToUpper(argc, &argv);
+	for (int i = 1; i < argc; i++) {
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
+	return ;
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) makeDefaultMegapehonSound();
+	else makeMegaphoneSound(argc, argv);
+	return (0);
+}
