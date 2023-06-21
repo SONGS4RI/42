14bbf5e58c0c3013a55b64de92312bc33e148b15
Jahlee <jahlee@student.42.fr>
2023-06-21 21:05:23 +0900

    cpp00 ex01 ing

--- cpp00/ex01/Phonebook.hpp
+++ cpp00/ex01/Phonebook.hpp
@@ -6,15 +6,14 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:31:23 by jahlee            #+#    #+#             */
-/*   Updated: 2023/06/21 18:00:23 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/06/21 20:25:49 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-# include <iostream>
-# include "./Contact.hpp"
+#include "./Contact.hpp"
 
 class Phonebook {
 	private:
@@ -26,5 +25,6 @@ class Phonebook {
 		Phonebook();
 		void addContact(Contact contact);
 		void getContacts();
+		void hi();
 };
 #endif
