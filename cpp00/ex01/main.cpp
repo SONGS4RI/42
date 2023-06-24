2a035cb2d8ba7f4a6bf483a0ec7ee127b9d88536
Jahlee <jahlee@student.42.fr>
2023-06-24 18:56:49 +0900

    cpp00 ex01 ing

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,32 +6,39 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 17:02:08 by jahlee            #+#    #+#             */
-/*   Updated: 2023/06/21 21:05:05 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/06/24 18:34:04 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./Phonebook.hpp"
+#include "./includes.hpp"
 
-
+void leaks(void) {
+	system("leaks ex01");
+}
 
 int main() {
+	atexit(leaks);
 	Phonebook newPhonebook = Phonebook();
-
-	newPhonebook.hi();
-	
 	std::string input;
-	while (std::getline(std::cin, input)) {
+
+	while (42) {
+		newPhonebook.printMessage(PROMPT, BLUE);
+		std::getline(std::cin, input);
+		if (input == "") {
+			newPhonebook.printMessage(EXIT, YELLOW);
+			break ;
+		}
 		for (int i = 0; i < input.size(); i++)
 			if (isupper(input[i])) input[i] = tolower(input[i]);
 		if (input == "add") {
-			std:: cout << "add" << std::endl;
+			newPhonebook.commandAdd();
 		} else if (input == "search") {
-			std:: cout << "search" << std::endl;
+			newPhonebook.commandSearch();
 		} else if (input == "exit") {
-			std:: cout << "exit" << std::endl;
+			newPhonebook.commandExit();
 			break ;
 		} else {
-			std:: cout << "Wrong Action!!!" << std::endl;
+			newPhonebook.printMessage(WRONG, RED);
 		}
 	}
 }
