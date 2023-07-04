2e133dfbe041f9e14cd0e232cf67ee58e7e352a5
Jahlee <jahlee@student.42.fr>
2023-07-04 19:47:56 +0900

    cpp01 ex05 ing

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 18:14:57 by jahlee            #+#    #+#             */
+/*   Updated: 2023/07/04 19:19:47 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain(std::string level);
+};
+
+#endif
