b6d37c6fb9c86a9f8f9cdfaa38e458b0058815d4
Jahlee <jahlee@student.42.fr>
2023-07-04 17:58:53 +0900

    cpp01 ex04 done

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 15:55:23 by jahlee            #+#    #+#             */
+/*   Updated: 2023/07/04 17:51:57 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv) {
+	if (argc != 4) {
+		std::cerr << "argv error" << std::endl;
+		return (1);
+	}
+	std::string filename(argv[1]);
+	std::string s1(argv[2]);
+	std::string s2(argv[3]);
+	std::ifstream ifs(filename, std::ios::in);
+
+	if (ifs.fail()) {
+		std::cerr << "open error" << std::endl;
+		return (1);
+	}
+	std::ofstream ofs(filename + ".replace", std::ios::out | std::ios::trunc);
+	if (ofs.fail()) {
+		std::cerr << "file make error" << std::endl;
+	}
+
+	std::string input;
+	int s1_size = s1.size();
+	while (std::getline(ifs, input)) {
+		int pos = -s1_size;
+		while ((pos = input.find(s1, pos + s1_size)) != -1) {
+			input = input.substr(0, pos) + s2 + input.substr(pos + s1_size);
+		}
+		ofs << input;
+		if (!ifs.eof()) ofs << std::endl;
+	}
+	ifs.close();
+	ofs.close();
+	return (0);
+}
