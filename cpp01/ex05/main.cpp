bc355cbbded8ca40b51a82bdc0f0d96dcf4c9307
Jahlee <jahlee@student.42.fr>
2023-07-05 16:07:09 +0900

    cpp01 done

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,18 +6,18 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 14:29:51 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/05 15:15:48 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/05 15:52:18 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Harl.hpp"
 
 void leaks(void) {
-	system("leaks ex05");
+	std::system("leaks ex05");
 }
 
 int main(void) {
-	atexit(leaks);
+	std::atexit(leaks);
 	Harl newHarl = Harl();
 
 	newHarl.complain("debug");
