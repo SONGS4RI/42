c590ef250fd78feb211f5ff4e7f2f8ecd3abcae2
Jahlee <jahlee@student.42seoul.kr>
2023-07-13 20:46:23 +0900

    cpp02 ex01 ing

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 19:02:57 by jahlee            #+#    #+#             */
+/*   Updated: 2023/07/13 17:12:22 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cmath>
+
+class Fixed {
+	private:
+		int _value;
+		static const int _bits = 8;
+	public:
+		Fixed(void);
+		Fixed(const int num);
+		Fixed(const float num);// 32bit!
+		Fixed(const Fixed& obj);
+		Fixed& operator=(const Fixed& obj);
+		Fixed& operator<<(const Fixed& obj);
+		~Fixed(void);
+
+		void setRawBits(int const raw);
+		int getRawBits(void) const;
+		float toFloat(void) const;// converts the fixed-point value to a floating-point value.
+		int toInt(void) const;// converts the fixed-point value to an integer value.
+};
