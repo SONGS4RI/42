95ba1ec24db5808bb7d6407d6ac567cf566068bf
Jahlee <jahlee@student.42seoul.kr>
2023-07-19 17:49:08 +0900

    cpp02 ex01 ing

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:02:57 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/16 15:52:05 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/19 15:55:08 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class Fixed {
 		union SharedData;
 		Fixed(void);
 		Fixed(const int num);
-		Fixed(const float num);// 32bit!
+		Fixed(const float num);// 32bit
 		Fixed(const Fixed& obj);
 		Fixed& operator=(const Fixed& obj);
 		Fixed& operator<<(const Fixed& obj);
