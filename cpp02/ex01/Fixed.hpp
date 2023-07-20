a9866a99c8cd0a683fd69bfbb4281fd280b5e378
Jahlee <jahlee@student.42seoul.kr>
2023-07-20 20:54:24 +0900

    cpp02 ex01 done

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,11 +6,12 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:02:57 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/19 15:55:08 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/20 20:43:46 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cmath>
+#include <iostream>
 
 class Fixed {
 	private:
@@ -20,10 +21,9 @@ class Fixed {
 		union SharedData;
 		Fixed(void);
 		Fixed(const int num);
-		Fixed(const float num);// 32bit
+		Fixed(const float num);
 		Fixed(const Fixed& obj);
 		Fixed& operator=(const Fixed& obj);
-		Fixed& operator<<(const Fixed& obj);
 		~Fixed(void);
 
 		void setRawBits(int const raw);
@@ -31,3 +31,5 @@ class Fixed {
 		float toFloat(void) const;// converts the fixed-point value to a floating-point value.
 		int toInt(void) const;// converts the fixed-point value to an integer value.
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
