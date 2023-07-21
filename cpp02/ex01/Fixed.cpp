921cf0e7619a1678afd57d4ab6509190d881bc83
jahlee <jahlee@student.42.fr>
2023-07-21 18:22:29 +0900

    cpp02 ex01 changing code

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:10:14 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/21 16:57:44 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/21 18:22:16 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Fixed::Fixed() {
 
 Fixed::Fixed(const int num) {
 	std::cout << "Int constructor called" << std::endl;
-	int sign = (num >> 31) & 1 ;
+	int sign = num >> 31;
 
 	_value = (num << _bits);
 	if (sign) _value = _value | ((sign << _bits) - 1);
@@ -40,17 +40,21 @@ ex) -5가 들어왔다 생각
 Fixed::Fixed(const float num) {
 	std::cout << "Float constructor called" << std::endl;
 	SharedData data;
-	int sign, exponent, fractional_part;
+	int sign, exponent, inter_fraction, integer_part, fractional_part;
 
 	data.data_f = num;
-	sign = (data.data_i >> 31) & 1;
+	sign = data.data_i >> 31;
 	exponent = ((data.data_i >> 23) & ((1 << 8) - 1)) - 127;
-	fractional_part = data.data_i & ((1 << 23) - 1);
-	if ((23 - _bits - exponent) >= 32) {
-		_value = sign << 31;
-	} else {
-		_value = (sign << 31) | ((fractional_part | (1 << 23)) >> (23 - _bits - exponent));
+	inter_fraction = data.data_i & ((1 << 23) - 1);
+	if (23 - _bits - exponent > 32) inter_fraction = 0;
+	else inter_fraction = ((inter_fraction | (1 << 23)) >> (23 - _bits - exponent));
+	fractional_part = inter_fraction & ((1 << _bits) - 1);
+	integer_part = inter_fraction >> 8;
+	if (sign) {
+		integer_part = ~integer_part;
+		fractional_part = (~fractional_part & ((1 << _bits) - 1)) + 1;
 	}
+	_value = (integer_part << _bits) | fractional_part;
 }
 
 /*
@@ -100,6 +104,23 @@ void Fixed::setRawBits(int const raw) {
 
 float Fixed::toFloat(void) const {
 	SharedData data;
+	int sign, exponent, inter_fraction, integer_part, fractional_part;
+
+	sign = _value >> 31;
+	fractional_part = _value & ((1 << _bits) - 1);
+	integer_part = _value >> _bits;
+	if (sign) {
+		
+	}
+	//지수 구해야함! 구해서 합쳐주자
+	inter_fraction = (integer_part << _bits) | fractional_part;
+	//data.data_i = (sign << 31) | ((지수 + 127) << 23) | inter_fraction에서 맨앞하나 때고
+	return (data.data_f);
+}
+
+/*
+float Fixed::toFloat(void) const {
+	SharedData data;
 	int sign, fixed_point, exponent = -_bits - 1;
 	int fractional_part = 1;
 
@@ -114,6 +135,7 @@ float Fixed::toFloat(void) const {
 	data.data_i = (sign << 31) | ((127 + exponent) << 23) | (fractional_part << (23 - (exponent + _bits)));
 	return (data.data_f);
 }
+*/
 
 int Fixed::toInt(void) const {
 	return (roundf(toFloat()));
