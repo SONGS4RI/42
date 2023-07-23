3ee58c4234dcd22c6b680affc192768743937b97
jahlee <jahlee@student.42.fr>
2023-07-23 16:41:53 +0900

    cpp02 ex01 fixing integer input

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:52:17 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/21 14:49:39 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/23 16:30:39 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 int main( void ) {
 	// Fixed a;
-	// Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	std::cout << "raw: " << std::bitset<32>(c.getRawBits()) << std::endl;
+	Fixed const b( -10 );
+	// Fixed const c( 42.42f );
 	// Fixed const d( b );
 	// a = Fixed( 1234.4321f );
+	// std::cout << std::bitset<32>(-10) << "\n";
 	// std::cout << "a is " << a << std::endl;
-	// std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
+	std::cout << "b is " << b << std::endl;
+	// std::cout << "c is " << c << std::endl;
 	// std::cout << "d is " << d << std::endl;
 	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
+/*
+111111111111111111111110 11111111
+*/
