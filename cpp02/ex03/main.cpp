3b563c8f71a6c8fd3123c7a293ac2b7be3ab4205
Jahlee <jahlee@student.42seoul.kr>
2023-07-25 20:22:44 +0900

    cpp02 probably done

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,26 +6,25 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:52:17 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/25 17:03:45 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/25 20:20:44 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Point.hpp"
+bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main( void ) {
-	Fixed x1(1.0f), y1(0.0f), z1(0.0f);
-	Fixed x2(0.0f), y2(1.0f), z2(0.0f);
+	Fixed x1(0.0f), y1(0.0f), z1(0.0f);
+	Fixed x2(1.0f), y2(0.0f), z2(0.0f);
+	Fixed x3(0.0f), y3(1.0f), z3(0.0f);
+	Fixed x4(0.8f), y4(0.1f), z4(0.0f);
 
 	Point a(x1, y1, z1);
 	Point b(x2, y2, z2);
+	Point c(x3, y3, z3);
+	Point p(x4, y4, z4);
 
-	Point cross_ab = Point::crossProduct(a, b);
-	Point cross_ba = Point::crossProduct(b, a);
-	Fixed inner_ab = Point::innerProduct(cross_ab, cross_ba);
-	std::cout  << cross_ab.getXCoordinate() << " " << cross_ab.getYCoordinate() << " " << cross_ab.getZCoordinate() << std::endl;
-	std::cout  << cross_ba.getXCoordinate() << " " << cross_ba.getYCoordinate() << " " << cross_ba.getZCoordinate() << std::endl;
-	std::cout  << inner_ab << std::endl;
-
-	
+	if (bsp(a, b, c, p)) std::cout << "inside" << std::endl;
+	else std::cout << "outside" << std::endl;
 	return 0;
 }
