b843a1de0729529ef8e43fbd2fb2cc78f88636e1
Jahlee <jahlee@student.42seoul.kr>
2023-07-25 17:07:39 +0900

    cpp02 ex02 ex03 fixed

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:52:17 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/25 16:13:46 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/07/25 17:03:45 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ int main( void ) {
 	Point a(x1, y1, z1);
 	Point b(x2, y2, z2);
 
-	// Point vec_a = a.makeVector
-
 	Point cross_ab = Point::crossProduct(a, b);
-	Fixed inner_ab = Point::innerProduct(a, b);
+	Point cross_ba = Point::crossProduct(b, a);
+	Fixed inner_ab = Point::innerProduct(cross_ab, cross_ba);
 	std::cout  << cross_ab.getXCoordinate() << " " << cross_ab.getYCoordinate() << " " << cross_ab.getZCoordinate() << std::endl;
+	std::cout  << cross_ba.getXCoordinate() << " " << cross_ba.getYCoordinate() << " " << cross_ba.getZCoordinate() << std::endl;
 	std::cout  << inner_ab << std::endl;
+
+	
 	return 0;
 }
