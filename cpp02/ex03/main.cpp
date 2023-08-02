9097ddfb16b64c9972311fd3aaae02e8f60348f1
Jahlee <jahlee@student.42seoul.kr>
2023-08-02 18:17:06 +0900

    cpp02 fixed

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:52:17 by jahlee            #+#    #+#             */
-/*   Updated: 2023/07/25 20:20:44 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/02 18:16:53 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main( void ) {
-	Fixed x1(0.0f), y1(0.0f), z1(0.0f);
-	Fixed x2(1.0f), y2(0.0f), z2(0.0f);
-	Fixed x3(0.0f), y3(1.0f), z3(0.0f);
-	Fixed x4(0.8f), y4(0.1f), z4(0.0f);
-
-	Point a(x1, y1, z1);
-	Point b(x2, y2, z2);
-	Point c(x3, y3, z3);
-	Point p(x4, y4, z4);
+	Point a(0.0f, 0.0f);
+	Point b(1.0f, 0.0f);
+	Point c(0.0f, 1.0f);
+	Point p(0.1f, 0.1f);
 
 	if (bsp(a, b, c, p)) std::cout << "inside" << std::endl;
 	else std::cout << "outside" << std::endl;
