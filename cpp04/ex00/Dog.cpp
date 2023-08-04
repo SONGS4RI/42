9daccc1772b5c7703e7e4f16fcab186dda423a9e
Jahlee <jahlee@student.42seoul.kr>
2023-08-04 15:12:45 +0900

    cpp04 ex00 ing

---
+++ cpp04/ex00/Dog.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 15:09:37 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/04 15:10:16 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Dog.hpp"
+
+Dog::Dog() {
+	std::cout << "default constructor called " << "[Dog] " << std::endl;
+}
+
+Dog::Dog(const Dog& obj) {
+	std::cout << "Copy constructor called " << "[Dog] " << std::endl;
+	*this=obj;
+}
+
+Dog& Dog::operator=(const Dog& obj) {
+	std::cout << "Copy assignment operator called " << "[Dog] " << std::endl;
+	if (this != &obj) {
+		
+	}
+	return (*this);
+}
+
+Dog::~Dog() {
+	std::cout << "Destructor called " << "[Dog] " << std::endl;
+}
