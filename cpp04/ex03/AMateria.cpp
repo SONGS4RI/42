4526b2598e2b59f7f529b2136e21c84267eee1eb
Jahlee <jahlee@student.42seoul.kr>
2023-08-05 17:14:37 +0900

    cpp04 ex03 ing

--- cpp04/ex03/AMateria.cpp
+++ cpp04/ex03/AMateria.cpp
@@ -6,16 +6,12 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:20:20 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/05 15:20:21 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/05 16:54:14 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "AMateria.hpp"
-
-AMateria::AMateria() {
-    std::cout << "default constructor called " << "[AMateria]" << std::endl;
-}
 
 AMateria::AMateria(const AMateria& obj) {
     std::cout << "Copy constructor called " << "[AMateria]" << std::endl;
@@ -25,11 +21,21 @@ AMateria::AMateria(const AMateria& obj) {
 AMateria& AMateria::operator=(const AMateria& obj) {
     std::cout << "Copy assignment operator called " << "[AMateria]" << std::endl;
     if (this != &obj) {
-        
+        _type = obj.getType();
     }
     return (*this);
 }
 
 AMateria::~AMateria() {
     std::cout << "Destructor called " << "[AMateria]" << std::endl;
+}
+
+AMateria::AMateria(std::string const & type) {
+    std::cout << "default constructor called " << "[AMateria]" << std::endl;
+
+    _type = type;
+}
+
+void AMateria::setType(std::string const & type) {
+    _type = type;
 }
--- cpp04/ex03/Character.hpp
+++ cpp04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:31:11 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/05 16:07:54 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/05 17:09:02 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@
 
 class Character : public ICharacter {
     private:
-        AMateria *inventory[4];
-    public:
+        AMateria *_inventory[4];
+        std::string _name;
         Character(void);
+    public:
+        Character(const std::string& name);
         Character(const Character& obj);
         Character& operator=(const Character& obj);
         ~Character(void);
 
-        std::string const & getName() const;
+        const std::string& getName() const;
         void equip(AMateria* m);
         void unequip(int idx);
         void use(int idx, ICharacter& target);
