04e18bc36791b064ec73252c4efc08315915c428
Jahlee <jahlee@student.42seoul.kr>
2023-08-14 20:27:26 +0900

    cpp05 ex02 done

--- Circle5/cpp05/ex02/main.cpp
+++ Circle5/cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 19:21:37 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/14 20:17:20 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/14 20:27:13 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,41 +29,42 @@ int main() {
 		std::cout << "====================test1====================" << std::endl;
 		/* ShrubberyCreationForm: Required grades: sign 145, exec 137 */
 
-		form = new ShrubberyCreationForm("home");
+		// form = new ShrubberyCreationForm("home");
 		// test = new Bureaucrat("test", 146);
-		test = new Bureaucrat("test", 145);
+		// test = new Bureaucrat("test", 145);
 		// test = new Bureaucrat("test", 137);
-		test->signForm(*form);
-		test->executeForm(*form);
+		// test->signForm(*form);
+		// test->executeForm(*form);
 
-		delete form; delete test;
+		// delete form; delete test;
 		std::cout << "====================test2====================" << std::endl;
 		/* RobotomyRequestForm: Required grades: sign 72, exec 45 */
 
-		form = new RobotomyRequestForm("jack");
-		test = new Bureaucrat("test", 73);
+		// form = new RobotomyRequestForm("jack");
+		// test = new Bureaucrat("test", 73);
 		// test = new Bureaucrat("test", 72);
 		// test = new Bureaucrat("test", 45);
-		test->signForm(*form);
-		test->executeForm(*form);
+		// test->signForm(*form);
+		// test->executeForm(*form);
 
-		delete form; delete test;
+		// delete form; delete test;
 		std::cout << "====================test3====================" << std::endl;
 		/* PresidentialPardonForm: Required grades: sign 25, exec 5 */
 
-		form = new PresidentialPardonForm("daniel");
-		test = new Bureaucrat("test", 26);
+		// form = new PresidentialPardonForm("daniel");
+		// test = new Bureaucrat("test", 26);
 		// test = new Bureaucrat("test", 25);
 		// test = new Bureaucrat("test", 5);
-		test->signForm(*form);
-		test->executeForm(*form);
+		// test->signForm(*form);
+		// test->executeForm(*form);
 
-		delete form; delete test;
+		// delete form; delete test;
 	} catch (const std::exception* e) {
 		std::cout << e->what() << std::endl;
 		if (form) delete form;
 		if (test) delete test;
 		delete e;
 	}
+
 	return 0;
 }
