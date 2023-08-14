6a43eeefa6c4933bb53d9866c363d6b74bf98b6a
Jahlee <jahlee@student.42seoul.kr>
2023-08-14 14:56:51 +0900

    cpp05 ex02 start

---
+++ Circle5/cpp05/ex02/AForm.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 19:40:05 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/14 14:55:26 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+#include <string>
+#include "Bureaucrat.hpp"
+
+class AForm {
+	private:
+		const std::string _name;
+		bool _signature;
+		const unsigned int _sign_grade;
+		const unsigned int _execute_grade;
+		AForm(void);
+	public:
+		class GradeTooHighException : public std::exception {
+			const char* what(void) const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			const char* what(void) const throw();
+		};
+		AForm(const std::string& name);
+		AForm(const std::string& name, const unsigned int& sign_grade,
+			const unsigned int& execute_grade);
+		AForm(const AForm& obj);
+		AForm& operator=(const AForm& obj);
+		~AForm(void);
+
+		const std::string& getName(void) const;
+		const bool& getSignature(void) const;
+		const unsigned int& getSignGrade(void) const;
+		const unsigned int& getExecuteGrade(void) const;
+
+		void beSigned(const Bureaucrat& obj);
+};
+
+std::ostream& operator<<(std::ostream& os, const AForm& obj);
+
+#endif
---
+++ Circle5/cpp05/ex02/Bureaucrat.cpp
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 17:02:57 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/14 14:49:15 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Exception [Bureaucrat]: Grade (1 ~ 150) is too high");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Exception [Bureaucrat]: Grade (1 ~ 150) is too low");
+}
+
+Bureaucrat::Bureaucrat(std::string name) : _name(name) {
+	std::cout << "Default constructor called " << "[Bureaucrat]" << std::endl;
+
+	_grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name) {
+	std::cout << "Default constructor called " << "[Bureaucrat]" << std::endl;
+
+	if (grade == 0) throw GradeTooHighException();
+	else if (grade > 150) throw GradeTooLowException();
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& obj) : _name(obj.getName()) {
+	std::cout << "Copy constructor called " << "[Bureaucrat]" << std::endl;
+
+	*this = obj;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
+	std::cout << "Copy assignment operator called " << "[Bureaucrat]" << std::endl;
+	if (this != &obj) {
+		_grade = obj._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << "Destructor called " << "[Bureaucrat]" << std::endl;
+}
+
+const std::string& Bureaucrat::getName(void) const {
+	return (_name);
+}
+
+const unsigned int& Bureaucrat::getGrade(void) const {
+	return (_grade);
+}
+
+void Bureaucrat::increaseGrade(void) {
+	if (_grade == 1) throw GradeTooHighException();
+
+	_grade--;
+}
+
+void Bureaucrat::decreaseGrade(void) {
+	if (_grade == 150) throw GradeTooLowException();
+
+	_grade++;
+}
+
+void Bureaucrat::signForm(Form& obj) {
+	try {
+		obj.beSigned(*this);
+	} catch (std::exception& e) {
+		std::cout << _name << " couldn’t sign " << obj.getName()
+		<< " because ";
+		throw (Form::GradeTooLowException());
+	}
+	std::cout << _name << " signed " << obj.getName() << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj) {
+	os << obj.getName() << ", bureaucrat grade " << obj.getGrade() << ".";
+
+	return (os);
+}
