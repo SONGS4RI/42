384a49086fd23b12055f78545a0dac3169f39f70
Jahlee <jahlee@student.42seoul.kr>
2023-08-15 16:20:44 +0900

    cpp05 ex03 done, need to check

--- Circle5/cpp05/ex03/Intern.cpp
+++ Circle5/cpp05/ex03/Intern.cpp
@@ -6,16 +6,20 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 14:37:14 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/15 14:45:54 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/15 16:16:09 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
-const char* Intern::WrongFormException::what() const throw() {
-	return ("Exception [Intern]: Wrong Form");
+const char* Intern::WrongFormException::what(void) const throw() {
+	return ("Exception [Intern]: Worng Form");
 }
+
 Intern::Intern() {
 	std::cout << "Default constructor called " << "[Intern]" << std::endl;
 }
@@ -29,7 +33,7 @@ Intern::Intern(const Intern& obj) {
 Intern& Intern::operator=(const Intern& obj) {
 	std::cout << "Copy assignment operator called " << "[Intern]" << std::endl;
 	if (this != &obj) {
-		
+
 	}
 	return (*this);
 }
@@ -39,7 +43,21 @@ Intern::~Intern() {
 }
 
 AForm* Intern::makeForm(const std::string& form, const std::string& target) const {
-	if (/* form 없는 형식이면 */) {
-		
+	std::string form_name[3] = {"presidential pardon", "robotomy request", "shrubbery creation"};
+
+	int i = 0;
+	for (; i<3; i++) {
+		if (form == form_name[i]) break;
+	}
+	switch (i)
+	{
+		case 0:
+			return (new PresidentialPardonForm(target));
+		case 1:
+			return (new RobotomyRequestForm(target));
+		case 2:
+			return (new ShrubberyCreationForm(target));
+		default:
+			throw new WrongFormException();
 	}
 }
--- Circle5/cpp05/ex03/main.cpp
+++ Circle5/cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 19:21:37 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/14 20:27:13 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/15 16:19:01 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,53 @@
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
-
+#include "Intern.hpp"
 void leaks() {
-	system("leaks ex02");
+	system("leaks ex03");
 }
 
 int main() {
 	atexit(leaks);
 	AForm* form = NULL;
 	Bureaucrat* test = NULL;
-
+	Intern someRandomIntern;
 	try {
 		std::cout << "====================test1====================" << std::endl;
 		/* ShrubberyCreationForm: Required grades: sign 145, exec 137 */
 
-		// form = new ShrubberyCreationForm("home");
+		form = someRandomIntern.makeForm("shrubbery creation", "home");
 		// test = new Bureaucrat("test", 146);
 		// test = new Bureaucrat("test", 145);
-		// test = new Bureaucrat("test", 137);
-		// test->signForm(*form);
-		// test->executeForm(*form);
+		test = new Bureaucrat("test", 137);
+		test->signForm(*form);
+		test->executeForm(*form);
 
-		// delete form; delete test;
+		if (form) delete form; form = NULL;
+		if (test) delete test; test = NULL;
 		std::cout << "====================test2====================" << std::endl;
 		/* RobotomyRequestForm: Required grades: sign 72, exec 45 */
 
-		// form = new RobotomyRequestForm("jack");
-		// test = new Bureaucrat("test", 73);
+		form = someRandomIntern.makeForm("robotomy request", "jack");
+		test = new Bureaucrat("test", 73);
 		// test = new Bureaucrat("test", 72);
 		// test = new Bureaucrat("test", 45);
 		// test->signForm(*form);
 		// test->executeForm(*form);
 
-		// delete form; delete test;
+		if (form) delete form; form = NULL;
+		if (test) delete test; test = NULL;
 		std::cout << "====================test3====================" << std::endl;
 		/* PresidentialPardonForm: Required grades: sign 25, exec 5 */
 
-		// form = new PresidentialPardonForm("daniel");
-		// test = new Bureaucrat("test", 26);
+		form = someRandomIntern.makeForm("presidential pardon", "daniel");
+		test = new Bureaucrat("test", 26);
 		// test = new Bureaucrat("test", 25);
 		// test = new Bureaucrat("test", 5);
 		// test->signForm(*form);
 		// test->executeForm(*form);
 
-		// delete form; delete test;
+		if (form) delete form; form = NULL;
+		if (test) delete test; test = NULL;
 	} catch (const std::exception* e) {
 		std::cout << e->what() << std::endl;
 		if (form) delete form;
