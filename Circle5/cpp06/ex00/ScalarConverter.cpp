3d938622fb330ec2530631fc1d3f607d69bf0085
Jahlee <jahlee@student.42seoul.kr>
2023-08-16 19:13:58 +0900

    cpp06 ex00 ing

--- Circle5/cpp06/ex00/ScalarConverter.cpp
+++ Circle5/cpp06/ex00/ScalarConverter.cpp
@@ -6,19 +6,20 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:24:57 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/16 16:33:24 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/16 19:13:47 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 #include "ScalarConverter.hpp"
 
 void ScalarConverter::convert(std::string input) {
 
-	(void) input;
-	std::cout << "char : " << std::endl;
-	std::cout << "int : " << std::endl;
-	std::cout << "float : " << std::endl;
-	std::cout << "double: " << std::endl;
+	double num = atof(input.c_str());
+	std::cout << "char : " << static_cast<char>(num) << std::endl;
+	std::cout << "int : " << static_cast<int>(num) << std::endl;
+	std::cout << "float : " << static_cast<float>(num) << std::endl;
+	std::cout << "double: " << num << std::endl;
 }
 
