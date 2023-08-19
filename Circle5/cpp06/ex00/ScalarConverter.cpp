557ff5ae38113f7a66e84455bfd1189e72fcd9a1
Jahlee <jahlee@student.42seoul.kr>
2023-08-19 21:05:10 +0900

    cpp06 start

--- Circle5/cpp06/ex00/ScalarConverter.cpp
+++ Circle5/cpp06/ex00/ScalarConverter.cpp
@@ -6,49 +6,36 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:24:57 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/17 15:14:35 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/19 21:04:00 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstdlib>
-#include <cmath>// nan
 #include "ScalarConverter.hpp"
 
-void ScalarConverter::convert(std::string input) {
-	double converted;
-	if (input == "" || (!std::isdigit(input[0]) && input.size() > 1)) {
-		converted = nan("");
-	} else {
-		
-	}
-	// ScalarConverter::convertToChar(input);
-	// ScalarConverter::convertToInt(input);
-	// ScalarConverter::convertToFloat(input);
-	// ScalarConverter::convertToDouble(input);
+
+const char* ScalarConverter::CanNotConvertException::what() const throw() {
+	return ("Can Not Convert input exception");
 }
 
-// void ScalarConverter::convertToChar(std::string input) {
-// 	double num = strtod(input.c_str(), NULL);
-// 	std::cout << "char : " << num;
-// 	// if (num >= 256 || num < 0) {
-// 	// 	std::cout << "impossible" << std::endl;
-// 	// } else if (isspace(c) || iscntrl(c) || isblank(c)) {
-// 	// 	std::cout << "not displayable" << std::endl;
-// 	// } else {
-// 	// 	std::cout << static_cast<char>(c) << std::endl;
-// 	// }
-// }
+void ScalarConverter::convert(const std::string& input) {
+	EType type = detectType(input);
+	switch (type) {
+		case TYPE_CHAR:
+			break;
+		case TYPE_INT:
+			break;
+		case TYPE_FLOAT:
+			break;
+		case TYPE_DOUBLE:
+			break;
+		default:
+			throw(CanNotConvertException());
+			break;
+	}
+}
 
-// void ScalarConverter::convertToInt(std::string input) {
-// 	std::cout << "int : " << atoi(input.c_str()) << std::endl;
-// }
-
-// void ScalarConverter::convertToFloat(std::string input) {
-// 	std::cout << "float : " << atof(input.c_str()) << std::endl;
-// }
-
-// void ScalarConverter::convertToDouble(std::string input) {
-// 	std::cout << "double: " << atof(input.c_str()) << std::endl;
-// }
-
+EType ScalarConverter::detectType(const std::string& input) {
+	
+	return (TYPE_INT);
+}
