3ae463b76e640eacd8b61019ff8ae32764e3c3ee
Jahlee <jahlee@student.42seoul.kr>
2023-08-19 14:17:52 +0900

    cpp05 fixed

--- Circle5/cpp05/ex01/main.cpp
+++ Circle5/cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 19:21:37 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/15 14:49:29 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/19 13:43:47 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 #include "Form.hpp"
 
 int main() {
-	Bureaucrat A("A", 1);
-	Bureaucrat B("B", 150);
-
-	Form F1("F1", 1, 1);
-	Form F2("F2", 150, 150);
-
 	try {
+		Bureaucrat A("A", 1);
+		Bureaucrat B("B", 150);
+
+		Form F1("F1", 1, 1);
+		Form F2("F2", 150, 150);
+
+		std::cout << F1 << std::endl;
+		std::cout << F2 << std::endl;
+
 		A.signForm(F1);
 		A.signForm(F2);
 
--- Circle5/cpp05/ex02/main.cpp
+++ Circle5/cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 19:21:37 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/15 15:07:44 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/19 14:16:04 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,52 +22,53 @@ void leaks() {
 
 int main() {
 	atexit(leaks);
-	AForm* form = NULL;
+	AForm* form1 = NULL;
+	AForm* form2 = NULL;
+	AForm* form3 = NULL;
 	Bureaucrat* test = NULL;
 
 	try {
-		std::cout << "====================test1====================" << std::endl;
-		/* ShrubberyCreationForm: Required grades: sign 145, exec 137 */
 
-		form = new ShrubberyCreationForm("home");
-		test = new Bureaucrat("test", 146);
+		// test = new Bureaucrat("test", 146);
 		// test = new Bureaucrat("test", 145);
 		// test = new Bureaucrat("test", 137);
-		// test->signForm(*form);
-		// test->executeForm(*form);
+		// test = new Bureaucrat("test", 73);
+		// test = new Bureaucrat("test", 72);
+		// test = new Bureaucrat("test", 45);
+		// test = new Bureaucrat("test", 26);
+		// test = new Bureaucrat("test", 25);
+		// test = new Bureaucrat("test", 5);
 
-		if (form) delete form; form = NULL;
-		if (test) delete test; test = NULL;
+
+		std::cout << "====================test1====================" << std::endl;
+		/* ShrubberyCreationForm: Required grades: sign 145, exec 137 */
+		form1 = new ShrubberyCreationForm("home");
+		test->signForm(*form1);
+		test->executeForm(*form1);
+
 		std::cout << "====================test2====================" << std::endl;
 		/* RobotomyRequestForm: Required grades: sign 72, exec 45 */
 
-		form = new RobotomyRequestForm("jack");
-		test = new Bureaucrat("test", 73);
-		// test = new Bureaucrat("test", 72);
-		// test = new Bureaucrat("test", 45);
-		// test->signForm(*form);
-		// test->executeForm(*form);
+		form2 = new RobotomyRequestForm("jack");
+		test->signForm(*form2);
+		test->executeForm(*form2);
 
-		if (form) delete form; form = NULL;
-		if (test) delete test; test = NULL;
 		std::cout << "====================test3====================" << std::endl;
 		/* PresidentialPardonForm: Required grades: sign 25, exec 5 */
 
-		form = new PresidentialPardonForm("daniel");
-		test = new Bureaucrat("test", 26);
-		// test = new Bureaucrat("test", 25);
-		// test = new Bureaucrat("test", 5);
-		// test->signForm(*form);
-		// test->executeForm(*form);
+		form3 = new PresidentialPardonForm("daniel");
+		test->signForm(*form3);
+		test->executeForm(*form3);
 
-		if (form) delete form; form = NULL;
-		if (test) delete test; test = NULL;
 	} catch (const std::exception* e) {
 		std::cout << e->what() << std::endl;
-		if (form) delete form;
-		if (test) delete test;
 		delete e;
 	}
+
+	if (form1) delete form1;
+	if (form2) delete form2;
+	if (form3) delete form3;
+	if (test) delete test;
 
 	return 0;
 }
