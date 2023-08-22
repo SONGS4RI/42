06deb44a9627e2bcf36ac8be80520f9b7a6b43bc
Jahlee <jahlee@student.42seoul.kr>
2023-08-22 17:48:07 +0900

    cpp06 ex00 ing

--- Circle5/cpp06/ex00/ScalarConverter.hpp
+++ Circle5/cpp06/ex00/ScalarConverter.hpp
@@ -6,17 +6,12 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:19:39 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/21 18:42:03 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/22 17:47:53 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
-
-# define FLOAT_BITS_EXP 0x7F800000
-# define FLOAT_BITS_MANTISSA 0x007FFFFF
-# define DOUBLE_BITS_EXP 0x7FF0000000000000
-# define DOUBLE_BITS_MANTISSA 0x000FFFFFFFFFFFF
 
 #include <string>
 
@@ -26,28 +21,25 @@ enum EType {
 	TYPE_FLOAT,
 	TYPE_DOUBLE,
 	TYPE_NAN,
-	TYPE_INF
+	TYPE_FLOWS
 };
 
 class ScalarConverter {
 	private:
+		static std::string _input;
 		ScalarConverter(void);
 		ScalarConverter(const ScalarConverter& obj);
 		ScalarConverter& operator=(const ScalarConverter& obj);
 		~ScalarConverter(void);
 	public:
-		class CanNotConvertException : public std::exception {
-			const char* what(void) const throw();
-		};
-		static EType detectType(const std::string& input);
+		static EType detectType();
+		static EType checkFlows(const EType& type);
 		static void convert(const std::string& input);
 
-		static void convertToChar(double converted);
-		static void convertToInt(double converted);
-		static void convertToFloat(double converted);
-		static void convertToDouble(double converted);
-
-		
+		static char convertToChar(const EType& type);
+		static int convertToInt(const EType& type);
+		static float convertToFloat(const EType& type);
+		static double convertToDouble(const EType& type);
 };
 
 #endif
