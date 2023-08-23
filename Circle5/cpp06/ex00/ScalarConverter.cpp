9eaf0174ea6d2da3b45e3f14f0541d644956ff64
Jahlee <jahlee@student.42seoul.kr>
2023-08-23 17:30:38 +0900

    cpp06 ex00 probably done

--- Circle5/cpp06/ex00/ScalarConverter.cpp
+++ Circle5/cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 16:24:57 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/23 16:01:04 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/23 17:30:06 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,13 @@
 
 #define _IMPOSSIBLE "impossible"
 #define _NOT_DISPLAYABLE "Non displayable"
-#define _NOT_A_NUMBER_F "nanf"
-#define _NOT_A_NUMBER_D "nan"
+#define _NOT_A_NUMBER "nan"
 
-std::string ScalarConverter::_input = "";
+std::string ScalarConverter::_input;
 
 void ScalarConverter::convert(const std::string& input) {
-	std::cout.precision(30);
 	_input = input;
 	EType type = detectType();
-	switch (type) {
-		case TYPE_CHAR:
-			std::cout << "Type: Char" << std::endl;
-			break;
-		case TYPE_INT:
-			std::cout << "Type: Int" << std::endl;
-			break;
-		case TYPE_FLOAT:
-			std::cout << "Type: Float" << std::endl;
-			break;
-		case TYPE_DOUBLE:
-			std::cout << "Type: Double" << std::endl;
-			break;
-		case TYPE_NAN:
-			std::cout << "Type: NaN" << std::endl;
-			break;
-		default:
-			break;
-	}
 
 	std::cout << "char: ";
 	try {
@@ -54,7 +33,7 @@ void ScalarConverter::convert(const std::string& input) {
 	} catch (std::out_of_range& o) {
 		std::cout << _IMPOSSIBLE;
 	}
-	std::cout<< std::endl;
+	std::cout << std::endl;
 
 	std::cout << "int: ";
 	try {
@@ -64,56 +43,74 @@ void ScalarConverter::convert(const std::string& input) {
 	} catch (std::out_of_range& o) {
 		std::cout << _IMPOSSIBLE;
 	}
-	std::cout<< std::endl;
+	std::cout << std::endl;
 
 	std::cout << "float: ";
 	try {
-		std::cout << convertToFloat(type);
-		std::cout << "(11)";
-		if (convertToFloat(type) == std::floor(stof(_input))) std::cout << ".0";
+		float res_f = convertToFloat(type);
+		std::cout << res_f;
+		if (std::abs(res_f) < std::powf(10,6) &&
+			convertToFloat(type) == std::floor(stof(_input)) &&
+			std::abs(res_f) != 1.0f/0.0f) std::cout << ".0";
 		std::cout << "f";
 	} catch (const char* e) {
-		std::cout << e;
+		std::cout << e << "f";
 	} catch (std::out_of_range& o) {
 		std::cout << _IMPOSSIBLE;
 	}
-	std::cout<< std::endl;
+	std::cout << std::endl;
 
 	std::cout << "double: ";
 	try {
-		std::cout << convertToDouble(type);
-		if (convertToDouble(type) == std::floor(stod(_input))) std::cout << ".0";
+		double res_d = convertToDouble(type);
+		std::cout << res_d;
+		if (std::abs(res_d) < std::powf(10,6) &&
+			convertToDouble(type) == std::floor(stod(_input)) &&
+			std::abs(res_d) != 1.0/0.0) std::cout << ".0";
 	} catch (const char* e) {
 		std::cout << e;
 	} catch (std::out_of_range& o) {
 		std::cout << _IMPOSSIBLE;
 	}
-	std::cout<< std::endl;
+	std::cout << std::endl;
 }
 
 EType ScalarConverter::detectType() {
 	int input_size = _input.size();
 	double converted = std::strtod(_input.c_str(), NULL);
 	if (std::abs(converted) == 1.0 / 0.0) {
-		if (_input == "inff" || _input == "+inff" || _input == "-inff") return (TYPE_FLOAT);
+		if (_input[input_size-2] == 'f') {
+			std::cout << "Type: Float" << std::endl;
+			return (TYPE_FLOAT);
+		}
+		std::cout << "Type: Double" << std::endl;
 		return (TYPE_DOUBLE);
 	}
-	if (input_size == 1 && !std::isdigit(_input[0])) return (TYPE_CHAR);
-	int dot_idx = -1, f_idx = -1;
-	for (int i=0; i<input_size; i++) {
+	if (input_size == 1 && !std::isdigit(_input[0])) {
+		std::cout << "Type: Char" << std::endl;
+		return (TYPE_CHAR);
+	}
+	int dot_idx = -1, f_idx = -1, sign = 0;
+	if (_input[0] == '+' || _input[0] == '-') sign++;
+	for (int i=sign; i<input_size; i++) {
 		if (_input[i] == '.' && dot_idx == -1 && i != 0) {
 			dot_idx = i;
 		} else if (_input[i] == 'f' && i == input_size - 1 && dot_idx != -1) {
 			f_idx = i;
 		} else if (!std::isdigit(_input[i])) {
+			std::cout << "Type: NaN" << std::endl;
 			return (TYPE_NAN);
 		}
 	}
 	if (dot_idx == -1) {
+		std::cout << "Type: Int" << std::endl;
 		return (TYPE_INT);
-	} else if (f_idx != -1) {
+	}
+	if (f_idx != -1) {
+		std::cout << "Type: Float" << std::endl;
 		return (TYPE_FLOAT);
 	}
+	std::cout << "Type: Double" << std::endl;
 	return (TYPE_DOUBLE);
 }
 
@@ -124,29 +121,17 @@ char ScalarConverter::convertToChar(const EType& type) {
 			if (!std::isprint(_input[0])) throw (_NOT_DISPLAYABLE);
 			return (_input[0]);
 		case TYPE_INT:
-			try {
-				res_i = std::stoi(_input);
-			} catch (std::out_of_range& o) {
-				throw (_IMPOSSIBLE);
-			}
+			res_i = std::stoi(_input);
 			if (res_i < 0 || 256 <= res_i) throw (_IMPOSSIBLE);
 			if (!std::isprint(res_i)) throw (_NOT_DISPLAYABLE);
 			return (static_cast<char>(res_i));
 		case TYPE_FLOAT:
-			try {
-				res_f = std::stof(_input);
-			} catch (std::out_of_range& o) {
-				throw (_IMPOSSIBLE);
-			}
+			res_f = std::stof(_input);
 			if (res_f < 0 || 256 <= res_f) throw (_IMPOSSIBLE);
 			if (!std::isprint(res_f)) throw (_NOT_DISPLAYABLE);
 			return (static_cast<char>(res_f));
 		case TYPE_DOUBLE:
-			try {
-				res_d = std::stod(_input);
-			} catch (std::out_of_range& o) {
-				throw (_IMPOSSIBLE);
-			}
+			res_d = std::stod(_input);
 			if (res_d < 0 || 256 <= res_d) throw (_IMPOSSIBLE);
 			if (!std::isprint(res_d)) throw (_NOT_DISPLAYABLE);
 			return (static_cast<char>(res_d));
@@ -156,32 +141,21 @@ char ScalarConverter::convertToChar(const EType& type) {
 }
 
 int ScalarConverter::convertToInt(const EType& type) {
-	int res_i; float res_f; double res_d;
+	float res_f; double res_d;
 	switch (type) {
 		case TYPE_CHAR:
 			return (static_cast<int>(_input[0]));
 		case TYPE_INT:
-			try {
-				res_i = std::stoi(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
-			return (res_i);
+			return (std::stoi(_input));
 		case TYPE_FLOAT:
-			try {
-				res_i = std::stoi(_input);
-				res_f = std::stof(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
+			res_f = std::stof(_input);
+			if (std::abs(res_f) == 1.0f / 0.0) throw (_IMPOSSIBLE);
+			std::stoi(_input);
 			return (static_cast<int>(res_f));
 		case TYPE_DOUBLE:
-			try {
-				res_i = std::stoi(_input);
-				res_d = std::stod(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
+			res_d = std::stod(_input);
+			if (std::abs(res_d) == 1.0f / 0.0) throw (_IMPOSSIBLE);
+			std::stoi(_input);
 			return (static_cast<int>(res_d));
 		default:
 			throw (_IMPOSSIBLE);
@@ -189,36 +163,26 @@ int ScalarConverter::convertToInt(const EType& type) {
 }
 
 float ScalarConverter::convertToFloat(const EType& type) {
-	float res_f; double res_d;
+	double res_d;
 	switch (type) {
 		case TYPE_CHAR:
 			return (static_cast<float>(_input[0]));
 		case TYPE_INT:
 			return (static_cast<float>(std::stoi(_input)));
 		case TYPE_FLOAT:
-			try {
-				res_f = std::stof(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
-			return (res_f);
+			return (std::stof(_input));
 		case TYPE_DOUBLE:
-			try {
-				res_f = std::stof(_input);
-				res_d = std::stod(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
+			std::stof(_input);
+			res_d = std::stod(_input);
 			return (static_cast<float>(res_d));
 		case TYPE_NAN:
-			throw (_NOT_A_NUMBER_F);
+			throw (_NOT_A_NUMBER);
 		default:
 			throw (_IMPOSSIBLE);
 	}
 }
 
 double ScalarConverter::convertToDouble(const EType& type) {
-	double res_d;
 	switch (type) {
 		case TYPE_CHAR:
 			return (static_cast<double>(_input[0]));
@@ -227,14 +191,9 @@ double ScalarConverter::convertToDouble(const EType& type) {
 		case TYPE_FLOAT:
 			return (static_cast<double>(std::stof(_input)));
 		case TYPE_DOUBLE:
-			try {
-				res_d = std::stod(_input);
-			} catch (std::out_of_range& e) {
-				throw (_IMPOSSIBLE);
-			}
-			return (res_d);
+			return (std::stod(_input));
 		case TYPE_NAN:
-			throw (_NOT_A_NUMBER_D);
+			throw (_NOT_A_NUMBER);
 		default:
 			throw (_IMPOSSIBLE);
 	}
