90a50ceb0ce13e1ddf32a9b4bad386ca9d7d09b4
Jahlee <jahlee@student.42seoul.kr>
2023-08-25 18:31:23 +0900

    cpp07 ex01 done

--- Circle5/cpp07/ex01/iter.hpp
+++ Circle5/cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:27:02 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/25 17:33:15 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/25 18:30:52 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define ITER_HPP
 
 template <typename T, typename Func>
-void iter(T* address, unsigned int len, Func func) {
-	
+void iter(T* address, unsigned int len, Func& func) {
+	for (unsigned int i=0; i<len; i++) {
+		func(*(address + i));
+	}
 }
 
 #endif
--- Circle5/cpp07/ex01/main.cpp
+++ Circle5/cpp07/ex01/main.cpp
@@ -6,14 +6,30 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 16:10:21 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/25 16:27:15 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/25 18:30:53 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "whatever.hpp"
+#include "iter.hpp"
+
+template <typename T>
+void printSomething(T& t) {
+	std::cout << t << std::endl;
+}
 
 int main( void ) {
-	
+	int n = 5;
+	int arr_i[n];
+	char arr_c[n];
+
+	for (int i=0; i<n; i++) {
+		arr_i[i] = i;
+		arr_c[i] = i + 'a';
+	}
+	std::cout << "==================test1==================\n";
+	iter(arr_i, n, printSomething<const int>);
+	std::cout << "==================test2==================\n";
+	iter(arr_c, n, printSomething<char>);
 	return 0;
 }
