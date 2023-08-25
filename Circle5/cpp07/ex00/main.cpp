b53e35b9cac892b57819c957d49c20da2758221b
Jahlee <jahlee@student.42seoul.kr>
2023-08-25 16:22:09 +0900

    cpp07 ex00 done

---
+++ Circle5/cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 16:10:21 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/25 16:18:59 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "whatever.hpp"
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
+}
---
+++ Circle5/cpp07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 16:11:04 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/25 16:18:14 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b= tmp;
+}
+
+template <typename T>
+T min(T& a, T& b) {
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T& a, T& b) {
+	return (a > b ? a : b);
+}
+
+#endif
