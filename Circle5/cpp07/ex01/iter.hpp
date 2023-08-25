f869a9951bae7f70832d8d6a0b02ddaef0985911
Jahlee <jahlee@student.42seoul.kr>
2023-08-25 17:33:24 +0900

    cpp07 ing

---
+++ Circle5/cpp07/ex01/iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 16:27:02 by jahlee            #+#    #+#             */
+/*   Updated: 2023/08/25 17:33:15 by jahlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template <typename T, typename Func>
+void iter(T* address, unsigned int len, Func func) {
+	
+}
+
+#endif
