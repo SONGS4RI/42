a965215ac424fa03575fd8d4e2ad7081a9cd2751
jahlee <jahlee@student.42.fr>
2023-08-27 18:58:19 +0900

    cpp08 ex01 adding func, ex02 start

--- Circle5/cpp08/ex02/main.cpp
+++ Circle5/cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 15:41:40 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/27 18:57:00 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/08/27 17:58:48 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,6 @@
 #include "Span.hpp"
 
 int main() {
-	try {
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
 
 	try {
 		Span sp = Span(5);
@@ -34,10 +22,6 @@ int main() {
 		sp.addNumber(17);
 		sp.addNumber(9);
 		sp.addNumber(11);
-
-		std::vector<int> test;
-		for (int i=20; i<200; i+=3) test.push_back(i);
-		sp.addNumbers(test.begin(), test.end());
 		std::cout << sp.shortestSpan() << std::endl;
 		std::cout << sp.longestSpan() << std::endl;
 	} catch (std::exception& e) {
