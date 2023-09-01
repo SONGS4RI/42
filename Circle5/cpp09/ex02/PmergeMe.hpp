60b0102d078c1fc9f6dde80956368faf6146a050
Jahlee <jahlee@student.42seoul.kr>
2023-09-01 20:03:36 +0900

    cpp09 ex02 ing

--- Circle5/cpp09/ex02/PmergeMe.hpp
+++ Circle5/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:25:12 by jahlee            #+#    #+#             */
-/*   Updated: 2023/09/01 17:06:17 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/09/01 20:02:35 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,25 @@
 class PmergeMe {
 	private:
 		static PmergeMe* _ptr;
-		std::vector<int> _input_nums;
-		std::vector<int> _v;
-		std::deque<int> _dq;
+		std::vector<int> _before;
+		std::vector<int> _after;
+		std::pair<int,int> _v_time;
+		std::pair<int,int> _dq_time;
 		PmergeMe(void);
 		PmergeMe(char** argv);
 		PmergeMe(const PmergeMe& obj);
 		PmergeMe& operator=(const PmergeMe& obj);
+		static bool compare(const std::pair<int, int>& a, const std::pair<int, int>& b);
 	public:
 		~PmergeMe(void);
 		static PmergeMe* getPmergeMe(char** argv);
+		void sortVector(void);
+		void sortDeque(void);
+		template <typename T>
+		void fordJohnsonSort(T& mainChain, T&subChain) {
+			(void)mainChain;
+			(void)subChain;
+		};
 };
 
 #endif
