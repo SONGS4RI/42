3b98a56277ff11bf751e3fffdcc50353be01e6e6
Jahlee <jahlee@student.42seoul.kr>
2023-09-09 17:19:16 +0900

    cpp08 fixed

--- Circle5/cpp08/ex02/MutantStack.hpp
+++ Circle5/cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 18:01:18 by jahlee            #+#    #+#             */
-/*   Updated: 2023/08/29 15:00:19 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/09/09 17:18:31 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,16 @@ class MutantStack : public std::stack<T> {
 		reverse_iterator rend(void) {return (this->c.rend());};
 
 		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin(void) {return (this->c.begin());};
+		const_iterator end(void) {return (this->c.end());};
 		const_iterator cbegin(void) {return (this->c.cbegin());};
 		const_iterator cend(void) {return (this->c.cend());};
 
 		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 		const_reverse_iterator crbegin(void) {return (this->c.cbegin());};
 		const_reverse_iterator crend(void) {return (this->c.cend());};
-
+		const_reverse_iterator rbegin(void) {return (this->c.rbegin());};
+		const_reverse_iterator rend(void) {return (this->c.rend());};
 };
 
 #endif
