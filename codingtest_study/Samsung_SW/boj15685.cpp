50162060af87b311110882a03efc6665fe395306
Jahlee <jahlee@student.42seoul.kr>
2023-09-23 13:54:22 +0900

    boj

--- codingtest_study/Samsung_SW/boj15685.cpp
+++ codingtest_study/Samsung_SW/boj15685.cpp
@@ -2,42 +2,39 @@
 #include <vector>
 using namespace std;
 
-int n;
-int x, y, d, g, cnt;
-vector<vector<int>> board(101, vector<int>(101, 0));
-int dx[4] = { 1, 0, -1, 0 }, dy[4] = { 0, -1, 0, 1 };
+int n, x, y, d, g, answer;
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
+vector<vector<bool>> board(101, vector<bool>(101, false));
 
 void drawDragonCurve(vector<int>& dirs) {
-    int size = dirs.size();
-    for (int i = size - 1; i >= 0; i--) {
-        int temp = (dirs[i] + 1) % 4;
-        x += dx[temp];
-        y += dy[temp];
-        board[x][y] = 1;
-        dirs.push_back(temp);
+    for (int i=dirs.size()-1; i>=0; i--) {
+        int dir = (dirs[i]+1)%4;
+        dirs.push_back(dir);
+        x += dx[dir];
+        y += dy[dir];
+        board[x][y] = true;
     }
 }
 
 int main() {
     cin >> n;
-    while (n--) {
-        vector<int> dirs;
+    for (int i=0; i<n; i++) {
         cin >> x >> y >> d >> g;
-
-        board[x][y] = 1;
+        board[x][y] = true;
         x += dx[d];
         y += dy[d];
-        board[x][y] = 1;
-        dirs.push_back(d);
-        while (g-- >= 0) {
+        board[x][y] = true;
+        vector<int> dirs = {d};
+        while (g--) {
             drawDragonCurve(dirs);
         }
     }
-   for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
-            if (board[i][j] == 1 && board[i][j + 1] == 1 && board[i + 1][j + 1] && board[i + 1][j] == 1)
-                cnt++;
+    for (int i=0; i<100; i++) {
+        for (int j=0; j<100; j++) {
+            if (board[i][j] && board[i+1][j] && board[i][j+1] && board[i+1][j+1]) {
+                answer++;
+            }
         }
     }
-    cout << cnt << endl;
+    cout << answer << "\n";
 }
