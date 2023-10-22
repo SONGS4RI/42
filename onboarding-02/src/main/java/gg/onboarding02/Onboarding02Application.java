675c2d81a994049c62cd5b10c2ccd084fa517ac5
jahlee <jahlee@student.42.fr>
2023-10-22 17:49:11 +0900

    done

---
+++ onboarding-02/src/main/java/gg/onboarding02/Onboarding02Application.java
@@ -0,0 +1,13 @@
+package gg.onboarding02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onboarding02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onboarding02Application.class, args);
+	}
+
+}
---
+++ onboarding-02/src/test/java/gg/onboarding02/Onboarding02ApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.onboarding02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onboarding02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
