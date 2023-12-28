524bf9edf53440c7e0f75a84164969c22d6c0955
이자훈 <jahlee@student.42seoul.kr>
2023-12-28 18:44:21 +0900

    :hammer: refactor: vector merge sort 구현 중

--- Circle5/cpp09/ex02/PmergeMe.hpp
+++ Circle5/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jahlee <jahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:25:12 by jahlee            #+#    #+#             */
-/*   Updated: 2023/09/12 13:47:31 by jahlee           ###   ########.fr       */
+/*   Updated: 2023/12/28 18:38:55 by jahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,22 @@ class PmergeMe {
 		PmergeMe(char** argv);
 		PmergeMe(const PmergeMe& obj);
 		PmergeMe& operator=(const PmergeMe& obj);
+		
+		void getJacobsthalNumbers();
+		void isValidElements(void);
+
+		void mergeSortVector(std::vector<std::pair<int, int> >& container, int low, int high, int depth);
+		void mergeVector(std::vector<std::pair<int, int> >& container, int low, int high);
+		void fordJohnsonSortVector(std::vector<int>& mainChain, std::vector<int>& subChain);
+
+		void mergeSortList(std::list<std::pair<int, int> >& container, int size);
+		// void fordJohnsonSortList(std::list<int>& mainChain, std::list<int>& subChain);
 	public:
 		~PmergeMe(void);
 		static PmergeMe* getPmergeMe(char** argv);
-		void isValidElements(void);
-		
 		void sortVector(void);
-		void recursiveInsertSortVector(std::vector<std::pair<int, int> >& container, int size);
-		void fordJohnsonSortVector(std::vector<int>& mainChain, std::vector<int>& subChain);
-
 		void sortList(void);
-		void recursiveInsertSortList(std::list<std::pair<int, int> >& container, int size);
-		// void fordJohnsonSortList(std::list<int>& mainChain, std::list<int>& subChain);
-
-		const std::vector<int>& getJacobsthalNumbers();
+		
 };
 
 #endif
